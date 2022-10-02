48eff388ab8717761b758c46e34f14269041ef0c
Gustavo Santos <gefgu@hotmail.com>
2022-10-02 10:05:11 -0300

    chore: fix variable declaration

--- Sorts/InsertionSort.ts
+++ Sorts/InsertionSort.ts
@@ -9,7 +9,7 @@
 
 export const insertionSort = (arr: number[]): number[] => {
   for (let i = 1; i < arr.length; i++) {
-    let temp = arr[i];
+    const temp = arr[i];
     let j = i - 1;
     while (j >= 0 && arr[j] > temp) {
       arr[j + 1] = arr[j];
