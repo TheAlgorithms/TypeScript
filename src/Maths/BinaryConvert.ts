5482de68fb115597fd297dc93ed7c84e26b53934
jonatan <u8794616@bsmch.net>
2022-10-02 11:13:48 +0300

    refactor: return value to string

--- src/Maths/BinaryConvert.ts
+++ src/Maths/BinaryConvert.ts
@@ -2,21 +2,18 @@
  * @function BinaryConvert
  * @description Convert the decimal to binary.
  * @param {number} num - The input integer
- * @return {number} - Binary of num.
+ * @return {string} - Binary of num.
  * @see [BinaryConvert](https://www.programiz.com/javascript/examples/decimal-binary)
  * @example BinaryConvert(12) = 1100
  * @example BinaryConvert(12 + 2) = 1110
  */
 
-export const BinaryConvert = (num: number): number => {
-  let power = 1
-  let binary = 0
+export const BinaryConvert = (num: number): string => {
+  let binary = ''
 
   while (num !== 0) {
-    const rem = num % 2
+    binary = (num % 2) + binary
     num = Math.floor(num / 2)
-    binary = rem * power + binary
-    power *= 10
   }
 
   return binary
