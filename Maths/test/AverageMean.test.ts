c7f7784ac96d519fae4146253b4ff0bf5b0c0b46
orangegrove_1955 <orangegrove_1955@hotmail.com>
2022-10-04 10:30:04 +1100

    Added correction to documentation, formatted

--- Maths/test/AverageMean.test.ts
+++ Maths/test/AverageMean.test.ts
@@ -1,31 +1,31 @@
-import { AverageMean } from '../AverageMean';
+import { AverageMean } from "../AverageMean";
 
-describe('Tests for AverageMean', () => {
-    it('should be a function', () => {
-      expect(typeof AverageMean).toEqual('function')
-    })
-  
-    it('should throw error for invalid input', () => {
-      expect(() => AverageMean([])).toThrow()
-    })
-  
-    it('should return the mean of an array of consecutive numbers', () => {
-      const meanFunction = AverageMean([1, 2, 3, 4])
-      expect(meanFunction).toBe(2.5)
-    })
-  
-    it('should return the mean of an array of numbers', () => {
-      const meanFunction = AverageMean([10, 40, 100, 20])
-      expect(meanFunction).toBe(42.5)
-    })
+describe("Tests for AverageMean", () => {
+  it("should be a function", () => {
+    expect(typeof AverageMean).toEqual("function");
+  });
 
-    it('should return the mean of an array of decimal numbers', () => {
-        const meanFunction = AverageMean([1.3, 12.67, 99.14, 20])
-        expect(meanFunction).toBe(33.2775)
-      })
+  it("should throw error for invalid input", () => {
+    expect(() => AverageMean([])).toThrow();
+  });
 
-      it('should return the mean of an array of numbers, including negatives', () => {
-        const meanFunction = AverageMean([10, -40, 100, -20])
-        expect(meanFunction).toBe(12.5)
-      })
-  })
+  it("should return the mean of an array of consecutive numbers", () => {
+    const meanFunction = AverageMean([1, 2, 3, 4]);
+    expect(meanFunction).toBe(2.5);
+  });
+
+  it("should return the mean of an array of numbers", () => {
+    const meanFunction = AverageMean([10, 40, 100, 20]);
+    expect(meanFunction).toBe(42.5);
+  });
+
+  it("should return the mean of an array of decimal numbers", () => {
+    const meanFunction = AverageMean([1.3, 12.67, 99.14, 20]);
+    expect(meanFunction).toBe(33.2775);
+  });
+
+  it("should return the mean of an array of numbers, including negatives", () => {
+    const meanFunction = AverageMean([10, -40, 100, -20]);
+    expect(meanFunction).toBe(12.5);
+  });
+});
