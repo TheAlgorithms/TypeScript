1a8f9aaca66721838b992d5fb44cde1bd11996a7
Mohd Faisal <96825384+MohdFaisalBidda@users.noreply.github.com>
2022-10-07 14:28:46 +0530

    Update Fibonacci.test.ts
    indentation fixed

--- Maths/test/Fibonacci.test.ts
+++ Maths/test/Fibonacci.test.ts
@@ -1,14 +1,18 @@
 import {nthFibonacci} from '../Fibonacci';
 
 describe('nthFibonacci', () => {
+    test('should return correct value', () => {
+        expect(nthFibonacci(0)).toBe(0);
+        expect(nthFibonacci(1)).toBe(1);
+        expect(nthFibonacci(5)).toBe(5);
+        expect(nthFibonacci(4)).toBe(3);
+        expect(nthFibonacci(0)).toBe(0);
+  });
+});   
+
+        
+        
   
-test('should return correct value', () => {
+
   
-expect(nthFibonacci(0)).toBe(0);
-expect(nthFibonacci(1)).toBe(1);
-expect(nthFibonacci(5)).toBe(5);
-expect(nthFibonacci(4)).toBe(3);
-expect(nthFibonacci(0)).toBe(0);
-  
-});
-});
+
