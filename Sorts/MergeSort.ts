053519d79af1059af32560358d1fc702e96e702b
Rajat Garg <rajatgarg379@gmail.com>
2022-10-08 16:37:38 +0530

    Refactored code

--- Sorts/MergeSort.ts
+++ Sorts/MergeSort.ts
@@ -3,6 +3,16 @@
  * @description keeps on dividing the list into equal halves until it can no more be divided. By definition, if it is only one element in the list, it is sorted.
  * @see [Merge Sort](https://www.javatpoint.com/merge-sort)
  * @example MergeSort([8, 3, 5, 1, 4, 2]) = [1, 2, 3, 4, 5, 8]
+ * @Complexity_Analysis
+ * Space complexity - O(n)
+ * Time complexity 
+ *      Best case   -   O(nlogn)
+ *      Worst case  -   O(nlogn)
+ *      Average case -  O(nlogn)
+ * 
+ * Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation. 
+ * T(n) = 2T(n/2) + O(n) 
+ * The solution of the above recurrence is O(nLogn).
  */
 
  export const MergeSort = (items: number[]): number[] => {
@@ -14,36 +24,33 @@
         high = MergeSort(high);
     }
     return merge(low, high);
-}
+};
 
 export const merge = (low: number[], high: number[]): number[] => {
     let indexLow = 0;
     let indexHigh = 0;
-    let lengthLow = low.length;
-    let lengthHigh = high.length;
-    let merged : number[] = [];
-    while (indexLow < lengthLow || indexHigh < lengthHigh) {
-        let lowItem = low[indexLow];
-        let highItem = high[indexHigh];
-        if (lowItem !== undefined) {
-            if (highItem === undefined) {
-                merged.push(lowItem);
-                indexLow++;
-            } else {
-                if (lowItem <= highItem) {
-                    merged.push(lowItem);
-                    indexLow++;
-                } else {
-                    merged.push(highItem);
-                    indexHigh++;
-                }
-            }
+    let curIndex = 0;
+    let merged = Array<number>(low.length + high.length);
+
+    while (indexLow < low.length && indexHigh < high.length) {
+
+        if (low[indexLow] <= high[indexHigh]) {
+            merged[curIndex++] = low[indexLow];
+            indexLow++;
         } else {
-            if (highItem !== undefined) {
-                merged.push(highItem);
-                indexHigh++;
-            }
+            merged[curIndex++] = high[indexHigh];
+            indexHigh++;
         }
     }
+
+    while (indexLow < low.length) {
+        merged[curIndex++] = low[indexLow];
+        indexLow++;
+    }
+
+    while (indexHigh < high.length) {
+        merged[curIndex++] = high[indexHigh];
+        indexHigh++;
+    }
     return merged;
-}
+};
--- Sorts/test/MergeSort.test.ts
+++ Sorts/test/MergeSort.test.ts
@@ -1,20 +1,12 @@
 import { MergeSort } from "../MergeSort";
 
-/**
- * Space complexity - O(n)
- * Time complexity 
- *      Best case   -   O(nlogn)
- *      Worst case  -   O(nlogn)
- *      Average case -  O(nlogn)
- * 
- * Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation. 
- * T(n) = 2T(n/2) + O(n) 
- * The solution of the above recurrence is O(nLogn).
- */
-
 describe("Merge Sort", () => {
-    it("should return the correct value", () => {
-        expect(MergeSort([1, 4, 2, 5, 9, 6, 3, 8, 10, 7])).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    });
+    it("generating array and comparing with sorted array", () => {
+        let arrLen = 10;
+        let inBuiltSortArr = Array.from(Array(arrLen)).map(x=>Math.random());
+        let mergeSortArray = inBuiltSortArr;
 
-  });
+        inBuiltSortArr.sort((a,b) => a-b);
+        expect(MergeSort(mergeSortArray)).toStrictEqual(inBuiltSortArr);
+    });
+});
