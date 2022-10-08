c27baa6974014752728c37fd145d713d9799a745
Rajat Garg <rajatgarg379@gmail.com>
2022-10-08 14:38:30 +0530

    Refactored code

--- Maths/PerfectSquare.ts
+++ Maths/PerfectSquare.ts
@@ -3,17 +3,9 @@
  * @description the square root of a number should be an integer
  * @see [Perfect square](https://www.cuemath.com/algebra/perfect-squares/)
  * @example Perfect square - 9, 16, 25
+ * @param {num} number
  */
 
-
-/**
- * @param {num} number 
- */
-
- export const PerfectSquare = (num : number) => {
-
-    let squareRoot:number = Math.sqrt(num)
-
-    return Number.isInteger(squareRoot)
-}
-
+export const PerfectSquare = (num: number) => {
+  return Number.isInteger(Math.sqrt(num));
+};
