9fb81324c7d8d3adc5ac08d9003b08c6ed3977c2
Rajat379 <51928008+Rajat379@users.noreply.github.com>
2022-10-08 19:37:51 +0530

    [Feat] : Added perfect square (#37)

---
+++ Maths/PerfectSquare.ts
@@ -0,0 +1,11 @@
+/**
+ * @function perfectSquare
+ * @description the square root of a number should be an integer
+ * @see [Perfect square](https://www.cuemath.com/algebra/perfect-squares/)
+ * @example Perfect square - 9, 16, 25
+ * @param {num} number
+ */
+
+export const PerfectSquare = (num: number) => {
+  return Number.isInteger(Math.sqrt(num));
+};
