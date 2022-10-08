93e79e452b410afe8824fefb26d788b324fbe4b2
Rajat Garg <rajatgarg379@gmail.com>
2022-10-08 15:19:31 +0530

    Refactor code

--- Maths/test/PerfectSquare.test.ts
+++ Maths/test/PerfectSquare.test.ts
@@ -1,11 +1,9 @@
 import { PerfectSquare } from "../PerfectSquare";
 
-describe("Perfect square", () => {
-  it("pass the test#1", () => {
+test("Check perfect square", () => {
     expect(PerfectSquare(16)).toBe(true);
-  });
-
-  it("pass the test#2", () => {
     expect(PerfectSquare(12)).toBe(false);
-  });
+    expect(PerfectSquare(19)).toBe(false);
+    expect(PerfectSquare(25)).toBe(true);
+    expect(PerfectSquare(42)).toBe(false);
 });
