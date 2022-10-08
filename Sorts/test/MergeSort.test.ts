e9d42068ea256b4a5f4e5d2698b5a076e139883e
Rajat Garg <rajatgarg379@gmail.com>
2022-10-08 17:18:21 +0530

    Refactored code

--- Sorts/test/MergeSort.test.ts
+++ Sorts/test/MergeSort.test.ts
@@ -1,12 +1,25 @@
 import { MergeSort } from "../MergeSort";
 
 describe("Merge Sort", () => {
-    it("generating array and comparing with sorted array", () => {
+    it("generating array with length 10 and comparing with sorted array", () => {
         let arrLen = 10;
-        let inBuiltSortArr = Array.from(Array(arrLen)).map(x=>Math.random());
+
+        let inBuiltSortArr = Array<number>(arrLen)
+        for (let i = 0; i < arrLen; i++) { inBuiltSortArr[i] = Math.random() }
         let mergeSortArray = inBuiltSortArr;
 
-        inBuiltSortArr.sort((a,b) => a-b);
+        inBuiltSortArr.sort((a, b) => a - b);
+        expect(MergeSort(mergeSortArray)).toStrictEqual(inBuiltSortArr);
+    });
+
+    it("generating array with length 20000 and comparing with sorted array", () => {
+        let arrLen = 20000;
+
+        let inBuiltSortArr = Array<number>(arrLen)
+        for (let i = 0; i < arrLen; i++) { inBuiltSortArr[i] = Math.random() }
+        let mergeSortArray = inBuiltSortArr;
+
+        inBuiltSortArr.sort((a, b) => a - b);
         expect(MergeSort(mergeSortArray)).toStrictEqual(inBuiltSortArr);
     });
 });
