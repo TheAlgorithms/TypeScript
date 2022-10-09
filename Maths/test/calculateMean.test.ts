1d655b420596e91495e4a80c1914b3b7f59f6483
orangegrove1955 <38221472+orangegrove1955@users.noreply.github.com>
2022-10-09 23:07:59 +1100

    algorithm: average/mean (#30)

---
+++ Maths/test/calculateMean.test.ts
@@ -0,0 +1,31 @@
+import { calculateMean } from "../calculateMean";
+
+describe("Tests for AverageMean", () => {
+  it("should be a function", () => {
+    expect(typeof calculateMean).toEqual("function");
+  });
+
+  it("should throw error for invalid input", () => {
+    expect(() => calculateMean([])).toThrow();
+  });
+
+  it("should return the mean of an array of consecutive numbers", () => {
+    const meanFunction = calculateMean([1, 2, 3, 4]);
+    expect(meanFunction).toBe(2.5);
+  });
+
+  it("should return the mean of an array of numbers", () => {
+    const meanFunction = calculateMean([10, 40, 100, 20]);
+    expect(meanFunction).toBe(42.5);
+  });
+
+  it("should return the mean of an array of decimal numbers", () => {
+    const meanFunction = calculateMean([1.3, 12.67, 99.14, 20]);
+    expect(meanFunction).toBe(33.2775);
+  });
+
+  it("should return the mean of an array of numbers, including negatives", () => {
+    const meanFunction = calculateMean([10, -40, 100, -20]);
+    expect(meanFunction).toBe(12.5);
+  });
+});
