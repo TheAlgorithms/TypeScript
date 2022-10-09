262ef9db15172d1516bfbb67ae111bbfce92f2e2
Mohd Faisal <96825384+MohdFaisalBidda@users.noreply.github.com>
2022-10-09 15:19:43 +0000

    chore: fix spelling

---
+++ Maths/DegreesToRadians.ts
@@ -0,0 +1,11 @@
+/** * A function to get radians from the degrees 
+* @param {number} degree - The input integer 
+* @return {number} radians of degrees 
+* @example degreesToRadians(45) => 0.7853 | degreesToRadians(90) => 1.5708
+* @see https://en.m.wikipedia.org/wiki/Radian
+* @author MohdFaisalBidda <https://github.com/MohdFaisalBidda> 
+*/
+
+export const degreesToRadians =(degrees:number):number =>{
+  return degrees * Math.PI/180;
+}
--- Maths/test/DegreeToRadian.test.ts
+++
@@ -1,7 +0,0 @@
-import {degreeToRadian} from '../DegreeToRadian'; 
-  
- test("DegreeToRadian", () => {
-  expect(degreeToRadian(0)).toBe(0);
-  expect(degreeToRadian(45)).toBe(0.7853981633974483);
-  expect(degreeToRadian(90)).toBe(1.5707963267948966);
-});
