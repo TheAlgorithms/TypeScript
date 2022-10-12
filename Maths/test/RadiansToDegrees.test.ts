7106aab666fc9d23f584d560212f7fb8fd54795b
Mohd Faisal <96825384+MohdFaisalBidda@users.noreply.github.com>
2022-10-12 16:26:33 +0000

    fix: fixed the formatting

--- Maths/test/RadiansToDegrees.test.ts
+++ Maths/test/RadiansToDegrees.test.ts
@@ -1,7 +1,7 @@
 import {radiansToDegrees} from '../RadiansToDegrees'; 
   
-  test("RadiansToDegrees", () => {
-    expect(radiansToDegrees(0)).toBe(0);
-    expect(radiansToDegrees(0.7853981633974483)).toBe(45);
-    expect(radiansToDegrees(1.5707963267948966)).toBe(90);
+test("RadiansToDegrees", () => {
+  expect(radiansToDegrees(0)).toBe(0);
+  expect(radiansToDegrees(0.7853981633974483)).toBe(45);
+  expect(radiansToDegrees(1.5707963267948966)).toBe(90);
 });
