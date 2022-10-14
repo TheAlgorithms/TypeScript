46b9bfe6958ed345981fca9a5b9fe7414e8ae481
Mohd Faisal <biddafaisal@gmail.com>
2022-10-15 00:51:48 +0530

    Fixed spacing

--- Sorts/BubbleSort.ts
+++ Sorts/BubbleSort.ts
@@ -19,9 +19,9 @@
 
 export const bubbleSort = (arr: number[]): number[] => {
   
-  for(let i = 0; i < arr.length; i++) { 
-    for(let j = 0; j < arr.length-1; j++) { //iterating till the 2nd last element of array
-      if(arr[j] > arr[j+1]) {         //current indexed number > next indexed number
+  for (let i = 0; i < arr.length; i++) { 
+    for (let j = 0; j < arr.length-1; j++) { //iterating till the 2nd last element of array
+      if (arr[j] > arr[j+1]) {         //current indexed number > next indexed number
         let temp: number = arr[j];   //swapping two numbers
         arr[j] = arr[j+1];
         arr[j+1] = temp;
