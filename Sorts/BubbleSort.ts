96de4d8a96e19a5185589f0472f5e32c9c22fbbe
Mohd Faisal <96825384+MohdFaisalBidda@users.noreply.github.com>
2022-10-14 08:48:31 +0000

    chore: fixed formatting and added some comments

--- Sorts/BubbleSort.ts
+++ Sorts/BubbleSort.ts
@@ -1,12 +1,15 @@
 /** 
  * @function bubbleSort 
- * @description Bubble sort algorithm is simple and easy. In bubble sort every pair of adjacent value is compared and swap if the first value is greater than the second one. By this with every iteration the greatest value goes to the right side making itascending order.
+ * @description Bubble sort algorithm is simple and easy. In bubble sort every pair of adjacent value is compared and swap if the first value is greater than the second one. By this with every iteration the greatest value goes to the right side making it ascending order.This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.
  * @Complexity_Analysis 
  * Space complexity - O(1) 
  * Time complexity  
- *      Best case   -   O(n) 
- *      Worst case  -   O(n^2) 
+ *      Best case   -   O(n)
+ *                      The best case occurs when an array is already sorted. 
+ *      Worst case  -   O(n^2)
+ *                      The worst case occurs when an array is reverse sorted.
  *      Average case -  O(n^2)
+ *                      The average case occurs when an array is reverse sorted.
  *
  * @param {number[]} arr - The input array 
  * @return {number[]} - The sorted array. 
@@ -15,15 +18,15 @@
  */
 
 export const bubbleSort =(arr:number[]):number[] =>{
-
+  
  for(let i=0;i<arr.length;i++){
-    for(let j=0;j<arr.length-1;j++){
-      if(arr[j]>arr[j+1]){
-        let temp:number =arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
-      }
+   for(let j=0;j<arr.length-1;j++){
+     if(arr[j]>arr[j+1]){
+       let temp:number =arr[j];
+       arr[j]=arr[j+1];
+       arr[j+1]=temp;
+     }
    }
-}
-  return arr;
+ }
+ return arr;
 }
