3eaf2e2f35431d3f07f6bb5dbdb82aee922aeb4d
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2022-10-15 21:25:15 +0530

    Fix Indents

--- Maths/test/AliquotSum.test.ts
+++ Maths/test/AliquotSum.test.ts
@@ -1,5 +1,5 @@
 import { AliquotSum } from "../AliquotSum";
 
 test.each([[15, 9], [18, 21], [28, 28], [100, 117], [169, 14], [1729, 511], [15625, 3906]])("Aliquot Sum", (num, expected) => {
-	expect(AliquotSum(num)).toBe(expected)
+  expect(AliquotSum(num)).toBe(expected)
 })
