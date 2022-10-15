05880109d6882fc76e0071f0c64053e97efd5855
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2022-10-15 12:24:28 +0200

    Remove enclosing test block

--- Maths/test/ArmstrongNumber.test.ts
+++ Maths/test/ArmstrongNumber.test.ts
@@ -1,7 +1,5 @@
 import { ArmstrongNumber } from "../ArmstrongNumber"
 
-test('ArmstrongNumber', () => {
-  test.each([[9, true], [-310, false], [0, false], [407, true], [420, false], [92727, true], [13579, false]])('i is an Armstrong number or not', (num, expected) => {
-    expect(ArmstrongNumber(num)).toBe(expected)
-  })
+test.each([[9, true], [-310, false], [0, false], [407, true], [420, false], [92727, true], [13579, false]])('i is an Armstrong number or not', (num, expected) => {
+  expect(ArmstrongNumber(num)).toBe(expected)
 })
