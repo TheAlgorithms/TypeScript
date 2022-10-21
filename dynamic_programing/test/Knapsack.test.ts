98fa37b90f815b1d425c5ee64e9ca28a399d04f3
Rohit <RohitSinghEmail@protonmail.com>
2022-10-21 23:25:27 +0530

    used describe each

--- dynamic_programing/test/Knapsack.test.ts
+++ dynamic_programing/test/Knapsack.test.ts
@@ -1,36 +1,24 @@
 import { knapsack } from "../Knapsack";
 
+const cases: [number, number, number[], number[], number][] = [
+  [6, 15, [6, 5, 6, 6, 3, 7], [5, 6, 4, 6, 5, 2], 17],
+  [3, 50, [10, 20, 30], [60, 100, 120], 220],
+  [3, 8, [3, 4, 5], [30, 50, 60], 90],
+  [
+    5,
+    5,
+    [1, 1, 1, 1, 1],
+    [1000000000, 1000000000, 1000000000, 1000000000, 1000000000],
+    5000000000,
+  ],
+];
+
 describe("Knapsack Algorithm Test", () => {
-  it("test 1 : Should be equal to 17", () => {
-    var values = [5, 6, 4, 6, 5, 2];
-    var weights = [6, 5, 6, 6, 3, 7];
-    var totalWeight = 15;
-    var numberOfItems = values.length;
-    expect(knapsack(numberOfItems, totalWeight, weights, values)).toBe(17);
-  });
-
-  it("test 2 : Should be equal to 220", () => {
-    var values = [60, 100, 120];
-    var weights = [10, 20, 30];
-    var totalWeight = 50;
-    var numberOfItems = values.length;
-    expect(knapsack(numberOfItems, totalWeight, weights, values)).toBe(220);
-  });
-  it("test 3 : Should be equal to 90", () => {
-    var values = [30, 50, 60];
-    var weights = [3, 4, 5];
-    var totalWeight = 8;
-    var numberOfItems = values.length;
-    expect(knapsack(numberOfItems, totalWeight, weights, values)).toBe(90);
-  });
-  it("test 4 : Should be equal to 5000000000", () => {
-    var values = [1000000000, 1000000000, 1000000000, 1000000000, 1000000000];
-    var weights = [1, 1, 1, 1, 1];
-
-    var totalWeight = 5;
-    var numberOfItems = values.length;
-    expect(knapsack(numberOfItems, totalWeight, weights, values)).toBe(
-      5000000000
-    );
-  });
+  test.each(cases)(
+    "given %p items and %p total weight available, with weights %p and values %p, knapsack should return %p",
+    (numberOfItems, totalWeight, weights, values, expectedResult) => {
+      const result = knapsack(numberOfItems, totalWeight, weights, values);
+      expect(result).toBe(expectedResult);
+    }
+  );
 });
