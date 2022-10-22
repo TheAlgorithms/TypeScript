c1a50a837c56bd4b2e514349c4775ff6546963e8
Rohit <RohitSinghEmail@protonmail.com>
2022-10-22 13:00:04 +0530

    applied suggested changes

--- dynamic_programing/Knapsack.ts
+++ dynamic_programing/Knapsack.ts
@@ -1,55 +1,59 @@
 /**
  * @function knapsack
- * @description Given weights and values of n (numberOfItems) items, put these items in a knapsack of capacity totalWeight to get the maximum total value in the knapsack. In other words, given two integer arrays values[0..n-1] and weights[0..n-1] which represent values and weights associated with n items respectively. Also given an integer totalWeight which represents knapsack capacity, find out the maximum value subset of values[] such that sum of the weights of this subset is smaller than or equal to totalWeight. You cannot break an item, either pick the complete item or don’t pick it (0-1 property).
+ * @description Given weights and values of n (numberOfItems) items, put these items in a knapsack of capacity to get the maximum total value in the knapsack. In other words, given two integer arrays values[0..n-1] and weights[0..n-1] which represent values and weights associated with n items respectively. Also given an integer capacity which represents knapsack capacity, find out the maximum value subset of values[] such that sum of the weights of this subset is smaller than or equal to capacity. You cannot break an item, either pick the complete item or don’t pick it (0-1 property).
  * @Complexity_Analysis
  * Space complexity - O(1)
- * Time complexity
- *      Best case   -   O(numberOfItems * totalWeight)
- *      Average case   -   O(numberOfItems * totalWeight)
- *      Worst case   -   O(numberOfItems * totalWeight)
+ * Time complexity (independent of input) : O(numberOfItems * capacity)
  *
- * @return maximum value subset of values[] such that sum of the weights of this subset is smaller than or equal to totalWeight.
+ * @return maximum value subset of values[] such that sum of the weights of this subset is smaller than or equal to capacity.
  * @see [Knapsack](https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/)
  * @example knapsack(3, 8, [3, 4, 5], [30, 50, 60]) = 90
  */
 export const knapsack = (
-  numberOfItems: number,
-  totalWeight: number,
+  capacity: number,
   weights: number[],
   values: number[]
 ) => {
-  // Declaring a data structure to store calculated states/values
-  var dp: number[][] = new Array(numberOfItems + 1);
+  if (weights.length != values.length) {
+    throw new Error(
+      "weights and values arrays should have same number of elements"
+    );
+  }
 
-  for (var i = 0; i < dp.length; i++) {
+  const numberOfItems = weights.length;
+
+  // Declaring a data structure to store calculated states/values
+  let dp: number[][] = new Array(numberOfItems + 1);
+
+  for (let i = 0; i < dp.length; i++) {
     // Placing an array at each index of dp to make it a 2d matrix
-    dp[i] = new Array(totalWeight + 1);
+    dp[i] = new Array(capacity + 1);
   }
 
   // Loop traversing each state of dp
-  for (var i = 0; i < numberOfItems; i++) {
-    for (var j = 0; j <= totalWeight; j++) {
+  for (let i = 0; i < numberOfItems; i++) {
+    for (let j = 0; j <= capacity; j++) {
       if (i == 0) {
         if (j >= weights[i]) {
           // grab the first item if it's weight is less than remaining weight (j)
           dp[i][j] = values[i];
-          continue;
+        } else {
+          // if weight[i] is more than remaining weight (j) leave it
+          dp[i][j] = 0;
         }
-        // if weight[i] is more than remaining weight (j) leave it
-        dp[i][j] = 0;
-        continue;
-      }
-
-      if (j < weights[i]) {
-        // weight of current item (weights[i]) is more than remaining weight (j), leave the current item and just carry on previous items
+      } else if (j < weights[i]) {
+        // if weight of current item (weights[i]) is more than remaining weight (j), leave the current item and just carry on previous items
         dp[i][j] = dp[i - 1][j];
-        continue;
+      } else {
+        // select the maximum of (if current weight is collected thus adding it's value) and (if current weight is not collected thus not adding it's value)
+        dp[i][j] = Math.max(
+          dp[i - 1][j - weights[i]] + values[i],
+          dp[i - 1][j]
+        );
       }
-      // select the maximum of (if current weight is collected thus adding it's value) and (if current weight is not collected thus not adding it's value)
-      dp[i][j] = Math.max(dp[i - 1][j - weights[i]] + values[i], dp[i - 1][j]);
     }
   }
 
   // Return the final maximized value at last position of dp matrix
-  return dp[numberOfItems - 1][totalWeight];
+  return dp[numberOfItems - 1][capacity];
 };
--- dynamic_programing/test/Knapsack.test.ts
+++ dynamic_programing/test/Knapsack.test.ts
@@ -1,11 +1,10 @@
 import { knapsack } from "../Knapsack";
 
-const cases: [number, number, number[], number[], number][] = [
-  [6, 15, [6, 5, 6, 6, 3, 7], [5, 6, 4, 6, 5, 2], 17],
-  [3, 50, [10, 20, 30], [60, 100, 120], 220],
-  [3, 8, [3, 4, 5], [30, 50, 60], 90],
+const cases: [number, number[], number[], number][] = [
+  [15, [6, 5, 6, 6, 3, 7], [5, 6, 4, 6, 5, 2], 17],
+  [50, [10, 20, 30], [60, 100, 120], 220],
+  [8, [3, 4, 5], [30, 50, 60], 90],
   [
-    5,
     5,
     [1, 1, 1, 1, 1],
     [1000000000, 1000000000, 1000000000, 1000000000, 1000000000],
@@ -15,9 +14,9 @@ const cases: [number, number, number[], number[], number][] = [
 
 describe("Knapsack Algorithm Test", () => {
   test.each(cases)(
-    "given %p items and %p total weight available, with weights %p and values %p, knapsack should return %p",
-    (numberOfItems, totalWeight, weights, values, expectedResult) => {
-      const result = knapsack(numberOfItems, totalWeight, weights, values);
+    "given %p capacity available, with weights %p and values %p, knapsack should return %p",
+    (capacity, weights, values, expectedResult) => {
+      const result = knapsack(capacity, weights, values);
       expect(result).toBe(expectedResult);
     }
   );
