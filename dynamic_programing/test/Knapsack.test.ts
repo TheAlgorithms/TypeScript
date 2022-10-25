538abaccba5815768a4c2eca5ba7607d51acd5bf
RohitSingh107 <64142943+RohitSingh107@users.noreply.github.com>
2022-10-25 14:39:28 +0530

    algorithm: knapsack (#69)

---
+++ dynamic_programing/test/Knapsack.test.ts
@@ -0,0 +1,23 @@
+import { knapsack } from "../Knapsack";
+
+const cases: [number, number[], number[], number][] = [
+  [15, [6, 5, 6, 6, 3, 7], [5, 6, 4, 6, 5, 2], 17],
+  [50, [10, 20, 30], [60, 100, 120], 220],
+  [8, [3, 4, 5], [30, 50, 60], 90],
+  [
+    5,
+    [1, 1, 1, 1, 1],
+    [1000000000, 1000000000, 1000000000, 1000000000, 1000000000],
+    5000000000,
+  ],
+];
+
+describe("Knapsack Algorithm Test", () => {
+  test.each(cases)(
+    "given %p capacity available, with weights %p and values %p, knapsack should return %p",
+    (capacity, weights, values, expectedResult) => {
+      const result = knapsack(capacity, weights, values);
+      expect(result).toBe(expectedResult);
+    }
+  );
+});
