69275a2d5780433269ce3c52e7a823aa1f3e998b
Vinicius Novaes <vcn2@cin.ufpe.br>
2022-10-27 10:53:41 -0300

    Making suggested changes

--- Search/LinearSearch.ts
+++ Search/LinearSearch.ts
@@ -12,7 +12,7 @@
  */
 export const linearSearch = (array: any[], target: any): number => {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] == target) return i;
+    if (array[i] === target) return i;
   }
   return -1;
 }
