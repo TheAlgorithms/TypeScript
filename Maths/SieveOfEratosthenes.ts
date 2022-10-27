296e8f0b9ea04d202a0e59515d3a1d4642163289
Vinicius Novaes <vcn2@cin.ufpe.br>
2022-10-27 10:13:35 -0300

    algorithm: sieve of erastosthenes (#74)
    * Sieve Of Eatosthenes implementation
    
    * Test addition

---
+++ Maths/SieveOfEratosthenes.ts
@@ -0,0 +1,27 @@
+/**
+ * @function  SieveOfEratosthenes
+ * @description Find the prime numbers between 2 and n 
+ * @param {number} n - numbers set the limit that the algorithm needs to look to find the primes
+ * @return {number[]} - List of prime numbers
+ * @see https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes\
+ * @example SieveOfErastosthenes(5) = [2,3,5] 
+ * @example SieveOfErastosthenes(10) = [2,3,5,7]
+ */
+
+export function SieveOfEratosthenes(n: number): number[] {
+  if (n < 0 || !Number.isInteger(n)) {
+    throw new Error("Only natural numbers are supported");
+  }
+  const numbers = new Array<boolean>(n + 1)
+    .fill(true)
+  const primeNumbers: number[] = [];
+  for (let i = 2; i <= n; i++) {
+    if (numbers[i]) {
+      primeNumbers.push(i);
+      for (let j = i + i; j <= n; j += i) {
+        numbers[j] = false;
+      }
+    }
+  }
+  return primeNumbers;
+}
