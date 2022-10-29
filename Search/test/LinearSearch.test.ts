69767aac3bb12f3ba8e8025cd41ddc67b240827c
Vinicius Novaes <vcn2@cin.ufpe.br>
2022-10-29 03:24:55 -0300

    algorithm: linear search (#75)

---
+++ Search/test/LinearSearch.test.ts
@@ -0,0 +1,14 @@
+import { linearSearch } from "../LinearSearch";
+
+describe("Linear search", () => {
+  test.each([
+    [['o', 'b', 'c'], 'c', 2],
+    [[1, 2, 3, 4, 5], 4, 3],
+    [['s', 't', 'r', 'i', 'n', 'g'], 'a', -1]
+  ])(
+    "of %o , searching for %o, expected %i",
+    (array: any[], target: any, index: number) => {
+      expect(linearSearch(array, target)).toStrictEqual(index)
+    },
+  );
+});
