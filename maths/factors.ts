478bc0fe0e954c57b9dc6dc0b898dd94ea4cb435
Nishant Racherla <nishant.racherla@gmail.com>
2023-02-15 09:26:55 -0800

    fix(maths): renamed Factors to FindFactors and minor optimizations

--- maths/factors.ts
+++ maths/factors.ts
@@ -1,24 +1,27 @@
 /**
- * @function Factors
+ * @function FindFactors
  * @description Find all the factors of a natural number.
  * @param {number} num - A natural number.
  * @return {number[]} - An array of all the factors of given natural number.
  * @see https://en.wikipedia.org/wiki/Divisor
- * @example Factors(1) = [1]
- * @example Factors(4) = [1,2,4]
- * @example Factors(16) = [1,3,5,15]
+ * @example FindFactors(1) = [1]
+ * @example FindFactors(4) = [1,2,4]
+ * @example FindFactors(16) = [1,3,5,15]
  */
 
-export const Factors = (num: number): number[] => {
+export const FindFactors = (num: number): number[] => {
   if (num <= 0 || !Number.isInteger(num)) {
     throw new Error("only natural numbers are supported");
   }
 
   const res: number[] = [];
-  // iterate from 1 to num and push factors into the res array
-  for (let i: number = 0; i <= num; i++) {
+  // iterate from 1 to square root of num
+  // push factors into the res array
+  for (let i: number = 0; i <= Math.sqrt(num); i++) {
     if (num % i === 0) res.push(i);
   }
 
+  // add num to the result array
+  res.push(num);
   return res;
 };
--- maths/test/factors.test.ts
+++ maths/test/factors.test.ts
@@ -1,10 +1,10 @@
-import { Factors } from "../factors";
+import { FindFactors } from "../factors";
 
-describe("Factors", () => {
+describe("FindFactors", () => {
   test.each([-890, -5.56, -7, 0, 0.73, 4.2, NaN, -Infinity, Infinity])(
     "should throw an error for non natural number %d",
     (num) => {
-      expect(() => Factors(num)).toThrowError(
+      expect(() => FindFactors(num)).toThrowError(
         "only natural numbers are supported",
       );
     },
@@ -13,7 +13,7 @@ describe("Factors", () => {
   test.each([[1, [1]], [2, [1,2]], [4, [1,2,4]], [6, [1,2,3,6]], [16, [1,2,4,8,16]]])(
     "of %i should return the correct array of its factors",
     (num, expected) => {
-      expect(Factors(num)).toStrictEqual(expected);
+      expect(FindFactors(num)).toStrictEqual(expected);
     },
   );
 });
