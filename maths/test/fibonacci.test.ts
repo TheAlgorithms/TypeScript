1422a5b61a2aa9502d370e971f47e851bfc0ac3f
zFlxw <lxcalflxw@gmail.com>
2023-03-05 15:52:46 +0100

    fix: improve tests

--- maths/test/fibonacci.test.ts
+++ maths/test/fibonacci.test.ts
@@ -1,35 +1,36 @@
-import {nthFibonacci, nthFibonacciRecursively} from '../fibonacci';
+import { nthFibonacci, nthFibonacciRecursively } from '../fibonacci';
 
 describe('nthFibonacci', () => {
-  it('should return the value of 0', () => {
-    expect(nthFibonacci(0)).toBe(0);
+  it.each([ true, false ])('should return the value of 0', (recursive: boolean) => {
+    if (recursive) {      
+      expect(nthFibonacciRecursively(0)).toBe(0);
+    } else {      
+      expect(nthFibonacci(0)).toBe(0);
+    }
   });
 
-  it('should return the value of 1', () => {        
-    expect(nthFibonacci(1)).toBe(1);
-    expect(nthFibonacci(2)).toBe(1);
+  it.each([true, false])('should return the value of 1', (recursive: boolean) => {
+    if (recursive) {
+      expect(nthFibonacciRecursively(1)).toBe(1);
+      expect(nthFibonacciRecursively(2)).toBe(1);
+    } else {
+      expect(nthFibonacci(1)).toBe(1);
+      expect(nthFibonacci(2)).toBe(1);
+    }
   });
 
-  it('should return the value from the fibonacci sequence', () => {
-    expect(nthFibonacci(5)).toBe(5);
-    expect(nthFibonacci(10)).toBe(55);
-    expect(nthFibonacci(15)).toBe(610);
-  });
-});
-
-describe('nthFibonacciRecursively', () => {
-  it('should return the value of 0', () => {
-    expect(nthFibonacciRecursively(0)).toBe(0);
-  });
-
-  it('should return the value of 1', () => {
-    expect(nthFibonacciRecursively(1)).toBe(1);
-    expect(nthFibonacciRecursively(2)).toBe(1);
-  });
-
-  it('should return the value from the fibonacci sequence', () => {
-    expect(nthFibonacciRecursively(5)).toBe(5);
-    expect(nthFibonacciRecursively(10)).toBe(55);
-    expect(nthFibonacciRecursively(15)).toBe(610);
-  });
+  it.each([true, false])(
+    'should return the value from the fibonacci sequence',
+    (recursive: boolean) => {
+      if (recursive) {
+        expect(nthFibonacciRecursively(5)).toBe(5);
+        expect(nthFibonacciRecursively(10)).toBe(55);
+        expect(nthFibonacciRecursively(15)).toBe(610);
+      } else {
+        expect(nthFibonacci(5)).toBe(5);
+        expect(nthFibonacci(10)).toBe(55);
+        expect(nthFibonacci(15)).toBe(610);
+      }
+    },
+  );
 });
