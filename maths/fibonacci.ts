bf7442404fa95bd18d04df7742e542b89ecf8842
zFlxw <lxcalflxw@gmail.com>
2023-03-05 13:29:50 +0100

    feat(maths): improve fibonacci number function

--- maths/fibonacci.ts
+++ maths/fibonacci.ts
@@ -1,25 +1,22 @@
 /**
- * A function to get nth Fibonacci number
- * @param number The input integer
- * @return {number} Fibonacci number of `number`
+ * A function to get `n`th Fibonacci number.
+ * 
+ * @param n The index of the number in the Fibonacci sequence.
+ * @return The Fibonacci number on the `n`th index in the sequence.
+ * 
  * @example nthFibonacci(4) => 3 | nthFibonacci(6) => 8
  * @see https://en.m.wikipedia.org/wiki/Fibonacci_number
  * @author MohdFaisalBidda <https://github.com/MohdFaisalBidda>
+ * @author zFlxw <https://github.com/zFlxw>
  */
-
-export const nthFibonacci = (number: number): number => {
-  if (number < 0)  throw "Number should be greater than 0";
-  
-  if (number === 0)  return 0;
-
-  let a = 0, b = 1;
-
-  for (let i = 1; i < number; ++i) {
-    const c = a + b;
-
-    a = b;
-    b = c;
+export const nthFibonacci = (n: number): number => {
+  if (n <= 0) {
+    return 0;
   }
 
-  return b;
+  if (n <= 2) {
+    return 1;
+  }
+  
+  return nthFibonacci(n - 1) + nthFibonacci(n - 2);
 };
--- maths/test/fibonacci.test.ts
+++ maths/test/fibonacci.test.ts
@@ -1,11 +1,18 @@
 import {nthFibonacci} from '../fibonacci';
 
 describe('nthFibonacci', () => {
-  test('should return correct value', () => {        
-    expect(nthFibonacci(0)).toBe(0);  
-    expect(nthFibonacci(1)).toBe(1);
-    expect(nthFibonacci(5)).toBe(5);
-    expect(nthFibonacci(4)).toBe(3);
+  it('should return the value of 0', () => {
     expect(nthFibonacci(0)).toBe(0);
+  });
+
+  it('should return the value of 1', () => {        
+    expect(nthFibonacci(1)).toBe(1);
+    expect(nthFibonacci(2)).toBe(1);
+  });
+
+  it('should return the value from the fibonacci sequence', () => {
+    expect(nthFibonacci(5)).toBe(5);
+    expect(nthFibonacci(10)).toBe(55);
+    expect(nthFibonacci(15)).toBe(610);
   });
 });
