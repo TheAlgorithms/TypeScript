e9fdb9828e435ff18e289828cf87828e0f20a251
zFlxw <lxcalflxw@gmail.com>
2023-03-05 14:34:42 +0100

    feat(maths): makes recursive fibonacci an extra function

--- maths/fibonacci.ts
+++ maths/fibonacci.ts
@@ -1,22 +1,58 @@
 /**
- * A function to get `n`th Fibonacci number.
- * 
- * @param n The index of the number in the Fibonacci sequence.
- * @return The Fibonacci number on the `n`th index in the sequence.
- * 
+ * A function to get nth Fibonacci number.
+ *
+ * Time Complexity: linear (O(n))
+ *
+ * @param number The index of the number in the Fibonacci sequence.
+ * @return The Fibonacci number on the nth index in the sequence.
+ *
  * @example nthFibonacci(4) => 3 | nthFibonacci(6) => 8
  * @see https://en.m.wikipedia.org/wiki/Fibonacci_number
  * @author MohdFaisalBidda <https://github.com/MohdFaisalBidda>
- * @author zFlxw <https://github.com/zFlxw>
  */
-export const nthFibonacci = (n: number): number => {
-  if (n <= 0) {
+export const nthFibonacci = (number: number): number => {
+  if (number < 0) {
+    throw 'Number should be greater than 0';
+  }
+
+  if (number === 0) {
     return 0;
   }
 
-  if (n <= 2) {
+  let a = 0,
+    b = 1;
+  for (let i = 1; i < number; ++i) {
+    const c = a + b;
+
+    a = b;
+    b = c;
+  }
+
+  return b;
+};
+
+/**
+ * A function to get nth Fibonacci number recursively. **Note: This recursive approach increases the time complexity**
+ *
+ * Time Complexity: exponential (O(ϕ^n))
+ *
+ * @param number The index of the number in the Fibonacci sequence.
+ * @return The Fibonacci number on the nth index in the sequence.
+ *
+ * @example nthFibonacci(4) => 3 | nthFibonacci(6) => 8
+ * @see https://en.m.wikipedia.org/wiki/Fibonacci_number
+ * @author zFlxw <https://github.com/zFlxw>
+ */
+export const nthFibonacciRecursively = (number: number): number => {
+  if (number === 0) {
+    return 0;
+  }
+
+  if (number <= 2) {
     return 1;
   }
-  
-  return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+  return (
+    nthFibonacciRecursively(number - 1) + nthFibonacciRecursively(number - 2)
+  );
 };
--- maths/test/fibonacci.test.ts
+++ maths/test/fibonacci.test.ts
@@ -1,4 +1,4 @@
-import {nthFibonacci} from '../fibonacci';
+import {nthFibonacci, nthFibonacciRecursively} from '../fibonacci';
 
 describe('nthFibonacci', () => {
   it('should return the value of 0', () => {
@@ -14,5 +14,22 @@ describe('nthFibonacci', () => {
     expect(nthFibonacci(5)).toBe(5);
     expect(nthFibonacci(10)).toBe(55);
     expect(nthFibonacci(15)).toBe(610);
+  });
+});
+
+describe('nthFibonacciRecursively', () => {
+  it('should return the value of 0', () => {
+    expect(nthFibonacciRecursively(0)).toBe(0);
+  });
+
+  it('should return the value of 1', () => {
+    expect(nthFibonacciRecursively(1)).toBe(1);
+    expect(nthFibonacciRecursively(2)).toBe(1);
+  });
+
+  it('should return the value from the fibonacci sequence', () => {
+    expect(nthFibonacciRecursively(5)).toBe(5);
+    expect(nthFibonacciRecursively(10)).toBe(55);
+    expect(nthFibonacciRecursively(15)).toBe(610);
   });
 });
