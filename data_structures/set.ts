b256d1a9e2bc20e24ae383669e22012174f6a07e
zfl4wless <noah.bauditz@live.de>
2023-03-09 20:56:49 +0100

    structure: set

---
+++ data_structures/set.ts
@@ -0,0 +1,67 @@
+import { HashTable } from "./hashing/hash_table";
+
+/**
+ * This is an implementation of the Set data structure based on a HashTable.
+ *
+ * @template T The type of the elements in the set.
+ */
+export class Set<T> {
+    private hashTable: HashTable<T, null>;
+
+    constructor() {
+        this.hashTable = new HashTable();
+    }
+
+    /**
+     * Adds a new element to the set.
+     *
+     * @param value The value to add to the set.
+     */
+    add(value: T): void {
+        this.hashTable.set(value, null);
+    }
+
+    /**
+     * Removes an element from the set.
+     *
+     * @param value The value to remove from the set.
+     */
+    delete(value: T): void {
+        this.hashTable.delete(value);
+    }
+
+    /**
+     * Checks if the set contains a given value.
+     *
+     * @param value The value to check for.
+     * @returns Whether the set contains the value.
+     */
+    has(value: T): boolean {
+        return this.hashTable.has(value);
+    }
+
+    /**
+     * Removes all elements from the set.
+     */
+    clear(): void {
+        this.hashTable.clear();
+    }
+
+    /**
+     * Returns an array of all the values in the set.
+     *
+     * @returns An array of all the values in the set.
+     */
+    values(): T[] {
+        return this.hashTable.keys();
+    }
+
+    /**
+     * Returns the number of elements in the set.
+     *
+     * @returns The number of elements in the set.
+     */
+    getSize(): number {
+        return this.hashTable.getSize();
+    }
+}
