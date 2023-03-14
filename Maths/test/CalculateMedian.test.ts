e5cd2be77c721de72eef788c55961d663ad7cc18
zfl4wless <noah.bauditz@live.de>
2023-03-14 08:28:47 +0100

    feat: improvements & resolves requested changes

--- Maths/test/CalculateMedian.test.ts
+++ Maths/test/CalculateMedian.test.ts
@@ -1,22 +1,23 @@
 import { calculateMedian } from "../CalculateMedian";
 
 describe("Tests for CalculateMedian", () => {
-  it("should be a function", () => {
-    expect(typeof calculateMedian).toEqual("function");
-  });
+    it("should be a function", () => {
+        expect(typeof calculateMedian).toEqual("function");
+    });
 
-  it("should throw error for invalid input", () => {
-    expect(() => calculateMedian([])).toThrow();
-  });
+    it("should throw error for invalid input", () => {
+        expect(() => calculateMedian([])).toThrowError(
+            "Input array must contain at least one number."
+        );
+    });
 
-  it("should return the median of an array of numbers - even length", () => {
-    const medianFunction = calculateMedian([1, 2, 3, 4]);
-    expect(medianFunction).toBe(2.5);
-  });
+    it("should return the median of an array of numbers - even length", () => {
+        const medianFunction = calculateMedian([1, 2, 3, 4]);
+        expect(medianFunction).toBe(2.5);
+    });
 
-  it("should return the median of an array of numbers - odd length", () => {
-    const medianFunction = calculateMedian([1, 2, 3, 4, 6, 8, 9]);
-    expect(medianFunction).toBe(4);
-  });
-
+    it("should return the median of an array of numbers - odd length", () => {
+        const medianFunction = calculateMedian([1, 2, 3, 4, 6, 8, 9]);
+        expect(medianFunction).toBe(4);
+    });
 });
