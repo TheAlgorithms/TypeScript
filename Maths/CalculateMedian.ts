ea783b961d7ffeb1b65d2d9f4d46fba8047f4a9a
Noah <70114451+zFl4wless@users.noreply.github.com>
2023-03-15 16:38:55 +0100

    feat(maths): finishes calculate median (#114)
    * feat: add calculate median
    
    * Resolving comments
    
    * feat: improvements & resolves requested changes
    
    ---------
    
    Co-authored-by: syedjafer <syedjafer1997@gmail.com>

---
+++ Maths/CalculateMedian.ts
@@ -0,0 +1,25 @@
+/**
+ * @function calculateMedian
+ * @description This function will find the median value of an array of numbers.
+ * @param {number[]} numbers Sorted array of numeric values.
+ * @return {number} The median of input numbers.
+ * @see https://en.wikipedia.org/wiki/Median
+ * @example calculateMedian([1, 2, 4, 5, 8]) = 4
+ * @example calculateMedian([1, 2, 4, 5]) = 3
+ */
+
+export const calculateMedian = (numbers: number[]): number => {
+    if (numbers.length < 1) {
+        throw new TypeError("Input array must contain at least one number.");
+    }
+
+    const totalNumbers = numbers.length;
+
+    if (totalNumbers % 2 === 0) {
+        let index = totalNumbers / 2;
+        return (numbers[index - 1] + numbers[index]) / 2;
+    } else {
+        let index = (totalNumbers + 1) / 2;
+        return numbers[index - 1];
+    }
+};
