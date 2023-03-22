c1ca2c1568ac8e470fa7fd375b86c6d31fc047e0
zfl4wless <noah.bauditz@live.de>
2023-03-22 15:52:22 +0100

    feat(maths): improvements & resolved changes

--- maths/is_palindrome.ts
+++ maths/is_palindrome.ts
@@ -1,18 +1,22 @@
 /**
- * A function to see if a number is a Pallindrome
- * @param number The input integer
- * @return {boolean} True or False based on the integer
+ * A function to see if a number is a Palindrome.
+ *
+ * @param number The input number.
+ * @return {boolean} Wether the number is a Palindrome or not.
  * @example isPalindrome(12321) => true | isPalindrome(455) => false
  * @see https://en.wikipedia.org/wiki/Palindromic_number
  * @author FatimaChariwala <https://github.com/fatima-0000>
  */
 
 export const IsPalindrome = (number: number): boolean => {
-    let reverseString = "";
-    let stringValue = number.toString();
-    let length = stringValue.length - 1;
-    for (let i = length; i >= 0; --i) {
-        reverseString = reverseString + stringValue[i];
+    const stringValue = number.toString();
+    const length = stringValue.length - 1;
+
+    for (let i = 0; i <= length; i++) {
+        if (stringValue[i] !== stringValue[length - i]) {
+            return false;
+        }
     }
-    return stringValue == reverseString;
+
+    return true;
 };
--- maths/test/is_palindrome.test.ts
+++ maths/test/is_palindrome.test.ts
@@ -2,7 +2,7 @@ import { IsPalindrome } from "../is_palindrome";
 
 describe("IsPalindrome", () => {
   test.each([[5, true], [1234, false], [12321, true], [32.23, true], [31343, false]])(
-    "correct output for for %i",
+    "correct output for %i",
     (nums, expected) => {
         expect(IsPalindrome(nums)).toBe(expected);
       },
