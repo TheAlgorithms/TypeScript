7e23e1e75c82eb367bf3af6ee21ff74824adcec9
Noah <70114451+zFl4wless@users.noreply.github.com>
2023-03-26 06:46:22 +0200

    feat(maths): is palindrome number (#118)
    * Is Palindrome Number
    
    * Is Palindrome Number update
    
    * Added tests and updated Directory.md
    
    * updated Directory.md
    
    * feat(maths): improvements & resolved changes
    
    * feat: improves algorithm & adjusts tests
    
    * feat: documents limitation
    
    ---------
    
    Co-authored-by: Fatima <chariwalafm@gmail.com>

---
+++ maths/test/is_palindrome.test.ts
@@ -0,0 +1,10 @@
+import { IsPalindrome } from "../is_palindrome";
+
+describe("IsPalindrome", () => {
+  test.each([[5, true], [1234, false], [12321, true], [31343, false]])(
+    "correct output for %i",
+    (nums, expected) => {
+        expect(IsPalindrome(nums)).toBe(expected);
+      },
+  );
+});
