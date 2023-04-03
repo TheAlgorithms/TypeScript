e85ea1b427840f3a75cef88c58e526ffff07ca88
Maik <58326890+zFlxw@users.noreply.github.com>
2023-04-03 08:00:32 +0200

    feat: Pascal's Triangle (#121)
    * feat(data-struct): binary search tree
    
    * fix: improvements part 1
    
    * fix: improvements part 2
    
    * Fix failing tests
    
    * feat: pascals triangle
    
    ---------
    
    Co-authored-by: Lars Müller <34514239+appgurueu@users.noreply.github.com>

---
+++ maths/pascals_triangle.ts
@@ -0,0 +1,40 @@
+/**
+ * Pascal's Triangle is an array of binomial coefficients. It can be used for unwrapping terms like
+ * (a + b)^5.
+ * To construct Pascal's Triangle you add the numbers above the child entry together. Here are the first five rows:
+ *     1
+ *    1 1
+ *   1 2 1
+ *  1 3 3 1
+ * 1 4 6 4 1
+ *
+ * Time Complexity: quadratic (O(n^2)).
+ *
+ * @param n The exponent / The index of the searched row.
+ * @returns The nth row of Pascal's Triangle
+ * @see https://en.wikipedia.org/wiki/Pascal's_triangle
+ */
+export const pascalsTriangle = (n: number): number[] => {
+  let arr: number[][] = [];
+  for (let i: number = 0; i < n; i++) {
+    if (i === 0) {
+      arr.push([1]);
+      continue;
+    }
+
+    let lastRow: number[] = arr[i - 1];
+    let temp: number[] = [];
+    for (let j: number = 0; j < lastRow.length + 1; j++) {
+      if (j === 0 || j === lastRow.length) {
+        temp.push(1);
+        continue;
+      }
+
+      temp.push(lastRow[j - 1] + lastRow[j]);
+    }
+
+    arr.push(temp);
+  }
+
+  return arr[arr.length - 1];
+};
