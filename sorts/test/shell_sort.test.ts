82f59419f9dc91f2b2ad9e1e10c610ff60699be9
21Horza <im.shoo@yandex.ru>
2023-04-11 01:46:05 +0800

    fix: shell sort test

--- sorts/test/shell_sort.test.ts
+++ sorts/test/shell_sort.test.ts
@@ -6,7 +6,7 @@ describe("Shell Sort", () => {
   });
 
   it("should return the correct value for worst case", () => {
-    expect(shellSort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(shellSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 
   it("should return the correct value for best case", () => {
