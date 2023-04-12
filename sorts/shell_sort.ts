3787b3832ca64b119d8290df919b924e5855991a
Horza <81642088+21Horza@users.noreply.github.com>
2023-04-12 23:22:42 +0800

    feat: add shell sort (#124)
    * feat: add shell sort algorithm
    
    * feat: add shell sort algorithm
    
    * fix: shell sort test
    
    * Use shifts instead of floor(x / 2), add some comments
    
    ---------
    
    Co-authored-by: Lars Müller <34514239+appgurueu@users.noreply.github.com>

---
+++ sorts/shell_sort.ts
@@ -0,0 +1,31 @@
+/**
+ * @function shellSort
+ * @description Shell sort algorithm is the optimization for insertion sort algorithm.
+ * @Complexity_Analysis
+ * Space complexity - O(1)
+ * Time complexity
+ *      Best case   -   Ω(n log(n))
+ *      Worst case  -   O(n^2)
+ *      Average case -  O(n log(n)^2)
+ *
+ * @param {T[]} arr - The input array
+ * @return {Array<T>} - The sorted array.
+ * @see [Shell Sort] (https://www.geeksforgeeks.org/shellsort/)
+ * @example shellSort([4, 1, 8, 10, 3, 2, 5, 0, 7, 6, 9]) = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ */
+export function shellSort<T>(arr: T[]): Array<T> {
+    // start with the biggest gap, reduce gap twice on each step
+    for (let gap = arr.length >> 1; gap > 0; gap >>= 1) {
+        for (let i = gap; i < arr.length; i++) {
+            const temp = arr[i];
+            let j = i; // index for compared element on the left side
+            // shift larger elements down
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp; // place i-th element at appropriate position
+        }
+    }
+    return arr;
+}
