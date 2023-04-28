0b71410400a8e9a914ac89715cef4755ba59c30f
benben <z2266109@gmail.com>
2023-04-29 00:45:27 +0800

    fix: update merge_sort.ts

--- sorts/merge_sort.ts
+++ sorts/merge_sort.ts
@@ -5,13 +5,13 @@
  * @example MergeSort([8, 3, 5, 1, 4, 2]) = [1, 2, 3, 4, 5, 8]
  * @Complexity_Analysis
  * Space complexity - O(n)
- * Time complexity 
+ * Time complexity
  *      Best case   -   O(nlogn)
  *      Worst case  -   O(nlogn)
  *      Average case -  O(nlogn)
- * 
- * Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation. 
- * T(n) = 2T(n/2) + O(n) 
+ *
+ * Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
+ * T(n) = 2T(n/2) + O(n)
  * The solution of the above recurrence is O(nLogn).
  */
 
@@ -19,25 +19,31 @@ export function mergeSort(array: number[]): number[] {
   if (array.length <= 1) return array.slice()
 
   const midIndex = Math.floor(array.length / 2)
-  const leftArray = array.slice(0, midIndex)
-  const rightArray = array.slice(midIndex, array.length)
-  
-  return merge(mergeSort(leftArray), mergeSort(rightArray))
+  const left = array.slice(0, midIndex)
+  const right = array.slice(midIndex, array.length)
+
+  return merge(mergeSort(left), mergeSort(right))
 }
 
-function merge (array1: number[], array2: number[]): number[] {
-  const result = []
-  let index1 = 0
-  let index2 = 0
+function merge(left: number[], right: number[]): number[] {
+  const result = Array<number>(left.length + right.length)
+  let curIndex = 0
+  let leftIndex = 0
+  let rightIndex = 0
 
-  while (index1 < array1.length && index2 < array2.length) {
-    if (array1[index1] < array2[index2]) {
-      result.push(array1[index1++])
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result[curIndex++] = left[leftIndex++]
     } else {
-      result.push(array2[index2++])
+      result[curIndex++] = right[rightIndex++]
     }
   }
+  while (leftIndex < left.length) {
+    result[curIndex++] = left[leftIndex++]
+  }
+  while (rightIndex < right.length) {
+    result[curIndex++] = right[rightIndex++]
+  }
 
-  return result.concat(array1.slice(index1)).concat(array2.slice(index2))
+  return result
 }
-
