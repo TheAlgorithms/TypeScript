b12c82e6ad0ed231e42ceea591dafca296e13917
benben <z2266109@gmail.com>
2023-04-28 18:54:40 +0800

    fix: merge_sort.ts and mroe
    - fix merge_sort with return copy of array
    - rename merge_sort.ts `MergeSrot` => `mergeSort`
    - rename quick_sort.ts `QuickSrot` => `quickSort`
    - rename merge_sort.test.ts `MergeSrot` => `mergeSort`
    - rename quick_sort.test.ts `QuickSrot` => `quickSort`

--- sorts/merge_sort.ts
+++ sorts/merge_sort.ts
@@ -14,10 +14,10 @@
  * T(n) = 2T(n/2) + O(n) 
  * The solution of the above recurrence is O(nLogn).
  */
-                                   
+
 export function mergeSort(array: number[]): number[] {
-  if (array.length <= 1) return array
-  
+  if (array.length <= 1) return array.slice()
+
   const midIndex = Math.floor(array.length / 2)
   const leftArray = array.slice(0, midIndex)
   const rightArray = array.slice(midIndex, array.length)
@@ -29,7 +29,7 @@ function merge (array1: number[], array2: number[]): number[] {
   const result = []
   let index1 = 0
   let index2 = 0
-  
+
   while (index1 < array1.length && index2 < array2.length) {
     if (array1[index1] < array2[index2]) {
       result.push(array1[index1++])
@@ -37,6 +37,7 @@ function merge (array1: number[], array2: number[]): number[] {
       result.push(array2[index2++])
     }
   }
-  
-  return [...result, ...array1.slice(index1), ...array2.slice(index2)]
+
+  return result.concat(array1.slice(index1)).concat(array2.slice(index2))
 }
+
