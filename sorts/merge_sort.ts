2603b2245ce073e18f096d3484335eb12b9d072c
Benben Weng <61361198+benben6515@users.noreply.github.com>
2023-05-01 14:40:00 +0800

    feat: update merge_sort.ts (#126)
    * Update merge_sort.ts
    
    - use JavaScript ES6 syntax
    - use `let`, `const` instead of `var`
    - make it more "clean", I supposed
    
    * fix: merge_sort.ts and mroe
    
    - fix merge_sort with return copy of array
    - rename merge_sort.ts `MergeSrot` => `mergeSort`
    - rename quick_sort.ts `QuickSrot` => `quickSort`
    - rename merge_sort.test.ts `MergeSrot` => `mergeSort`
    - rename quick_sort.test.ts `QuickSrot` => `quickSort`
    
    * fix: update merge_sort.ts
    
    * fix: update merge_sort.ts
    
    * fix: convert naming of quick_sort.ts
    
    * fix: convert naming of quick_sort.ts

--- sorts/merge_sort.ts
+++ sorts/merge_sort.ts
@@ -5,52 +5,45 @@
  * @example MergeSort([8, 3, 5, 1, 4, 2]) = [1, 2, 3, 4, 5, 8]
  * @Complexity_Analysis
  * Space complexity - O(n)
- * Time complexity 
+ * Time complexity
  *      Best case   -   O(nlogn)
  *      Worst case  -   O(nlogn)
  *      Average case -  O(nlogn)
- * 
- * Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation. 
- * T(n) = 2T(n/2) + O(n) 
+ *
+ * Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
+ * T(n) = 2T(n/2) + O(n)
  * The solution of the above recurrence is O(nLogn).
  */
 
- export const MergeSort = (items: number[]): number[] => {
-    var halfLength = Math.ceil(items.length / 2);
-    var low = items.slice(0, halfLength);
-    var high = items.slice(halfLength);
-    if (halfLength > 1) {
-        low = MergeSort(low);
-        high = MergeSort(high);
+export function mergeSort(array: number[]): number[] {
+  if (array.length <= 1) return array.slice()
+
+  const midIndex = Math.floor(array.length / 2)
+  const left = array.slice(0, midIndex)
+  const right = array.slice(midIndex, array.length)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
+
+function merge(left: number[], right: number[]): number[] {
+  const result = Array<number>(left.length + right.length)
+  let curIndex = 0
+  let leftIndex = 0
+  let rightIndex = 0
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result[curIndex++] = left[leftIndex++]
+    } else {
+      result[curIndex++] = right[rightIndex++]
     }
-    return merge(low, high);
-};
+  }
+  while (leftIndex < left.length) {
+    result[curIndex++] = left[leftIndex++]
+  }
+  while (rightIndex < right.length) {
+    result[curIndex++] = right[rightIndex++]
+  }
 
-export const merge = (low: number[], high: number[]): number[] => {
-    let indexLow = 0;
-    let indexHigh = 0;
-    let curIndex = 0;
-    let merged = Array<number>(low.length + high.length);
-
-    while (indexLow < low.length && indexHigh < high.length) {
-
-        if (low[indexLow] <= high[indexHigh]) {
-            merged[curIndex++] = low[indexLow];
-            indexLow++;
-        } else {
-            merged[curIndex++] = high[indexHigh];
-            indexHigh++;
-        }
-    }
-
-    while (indexLow < low.length) {
-        merged[curIndex++] = low[indexLow];
-        indexLow++;
-    }
-
-    while (indexHigh < high.length) {
-        merged[curIndex++] = high[indexHigh];
-        indexHigh++;
-    }
-    return merged;
-};
+  return result
+}
