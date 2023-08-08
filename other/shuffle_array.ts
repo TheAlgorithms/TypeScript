5248c4265a1a18b25470041eec295ab1b8b6415f
Omer M <omerplays1411@gmail.com>
2023-08-08 15:08:15 +0300

    refactor: removed the description of shuffleArray

--- other/shuffle_array.ts
+++ other/shuffle_array.ts
@@ -1,9 +1,3 @@
-/**
- * @function shuffleArray
- * @description shuffles an array.
- * @param {number[]} arr - array to shuffle
- * @returns {void} - the function does not return anything
-*/
 export function shuffleArray(arr: number[]) {
 	for (let i = arr.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
