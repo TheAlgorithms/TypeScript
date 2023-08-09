a4c8d373a3168e13988fca770290bbee0e90f9d1
Omer M <125453822+mendlero@users.noreply.github.com>
2023-08-09 22:05:27 +0300

    added bogosort algorithem (#147)
    * added bogo sort algorithem
    
    * Revert "added bogo sort algorithem"
    
    This reverts commit 5a512df0cc7e726834d769ed347f4ade14a018a9.
    
    * feat: added bogosort algorithem
    
    * refactor: split bogosort into 2 seperate algorithems
    
    * test: added tests to shuffle_array and is_sorted_array
    
    * test: added test for bogo sort algorithem
    
    * refactor: changed the complexity analysis of the bogosort algorithem
    
    * test: updated the test to use test.each
    
    * refactor: removed the description of shuffleArray

---
+++ sorts/bogo_sort.ts
@@ -0,0 +1,29 @@
+import { isSortedArray } from '../other/is_sorted_array';
+import { shuffleArray } from '../other/shuffle_array';
+
+/**
+ * @function bogoSort
+ * @description bogo sort is very simple to understand, it randomly shuffeles the input array until it is sorted
+ * @Complexity_Analysis
+ * Space complexity - O(1)
+ * Time complexity
+ *      Best case   -   O(n)
+ *                      The best case occurs when the array is already sorted.
+ *      Worst case  -   unbounded
+ *                      The worst case occurs when the shuffles never make the array sorted.
+ *      Average case -  O(n!n)
+ *                      The average case occurs when the shuffles sort the array after
+ *                      n! iterations (every iteration has a probability of 1/n! to sort the array),
+ *                      each iteration takes O(n) time.
+ *
+ * @param {number[]} arr - The input array
+ * @return {number[]} - The sorted array.
+ * @see [Bogo Sort](https://en.wikipedia.org/wiki/Bogosort)
+ * @example bogoSort([8, 3, 5, 1, 4, 2]) = [1, 2, 3, 4, 5, 8]
+ */
+export function bogoSort(arr: number[]): number[] {
+	while (!isSortedArray(arr)) {
+		shuffleArray(arr);
+	}
+	return arr;
+}
