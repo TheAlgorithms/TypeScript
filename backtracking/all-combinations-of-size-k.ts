86001c7469d21aa47aeed48246f422fab5e3cc36
nebarf <francesco.benedetto263@gmail.com>
2023-08-18 11:50:01 +0200

    docs(backtracking): add docs

--- backtracking/all-combinations-of-size-k.ts
+++ backtracking/all-combinations-of-size-k.ts
@@ -1,3 +1,14 @@
+/**
+ * This generates an array of unique sub"sets" (represented by ascendingly sorted subarrays)
+ * of size k out of n+1 numbers from 1 to n.
+ *
+ * By using a backtracking algorithm we can incrementally build sub"sets" while dropping candidates
+ * that cannot contribute anymore to a valid solution.
+ * Steps:
+ * - From the starting number (i.e. "1") generate all combinations of k numbers.
+ * - Once we got all combinations for the given number we can discard it (“backtracks”)
+ *   and repeat the same process for the next number.
+ */
 export function generateCombinations(n: number, k: number): number[][] {
   return generateAllCombos(n, k, 1);
 }
