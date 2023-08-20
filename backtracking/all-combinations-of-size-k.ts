4f68cf0ae77f3ff7c401d31d1bdc22dbc7944649
nebarf <francesco.benedetto263@gmail.com>
2023-08-20 17:57:37 +0200

    refactor(backtracking): move recursive routine in closure

--- backtracking/all-combinations-of-size-k.ts
+++ backtracking/all-combinations-of-size-k.ts
@@ -10,28 +10,29 @@
  *   and repeat the same process for the next number.
  */
 export function generateCombinations(n: number, k: number): number[][] {
-  return generateAllCombos(n, k, 1);
-}
+  let combinationsAcc: number[][] = [];
+  let currentCombination: number[] = [];
 
-function generateAllCombos(
-  n: number,
-  k: number,
-  startCursor: number,
-  combinationsAcc: number[][] = [],
-  currentCombination: number[] = []
-): number[][] {
-  if (k === 0) {
-    if (currentCombination.length > 0) {
-      combinationsAcc.push(currentCombination.slice());
+  function generateAllCombos(
+    n: number,
+    k: number,
+    startCursor: number
+  ): number[][] {
+    if (k === 0) {
+      if (currentCombination.length > 0) {
+        combinationsAcc.push(currentCombination.slice());
+      }
+      return combinationsAcc;
+    }
+
+    const endCursor = n - k + 2;
+    for (let i = startCursor; i < endCursor; i++) {
+      currentCombination.push(i);
+      generateAllCombos(n, k - 1, i + 1);
+      currentCombination.pop();
     }
     return combinationsAcc;
   }
 
-  const endCursor = n - k + 2;
-  for (let i = startCursor; i < endCursor; i++) {
-    currentCombination.push(i);
-    generateAllCombos(n, k - 1, i + 1, combinationsAcc, currentCombination);
-    currentCombination.pop();
-  }
-  return combinationsAcc;
+  return generateAllCombos(n, k, 1);
 }
