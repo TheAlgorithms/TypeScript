c724381590024a622c3668a5f40ed78fe4d19e18
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-10-01 21:37:21 +0530

    feat : added is power of 4 method

---
+++ bit_manipulation/test/is_power_of_4.test.ts
@@ -0,0 +1,14 @@
+import { isPowerOfFour } from "../is_power_of_4"
+
+describe('IsPowerOfFour', () => {
+  it.each([
+    [0, false],
+    [4, true],
+    [16, true],
+    [12, false],
+    [64, true],
+    [-64, false]
+  ])('should return the number %i is power of four or not', (n, expected) => {
+    expect(isPowerOfFour(n)).toBe(expected)
+  })
+})
