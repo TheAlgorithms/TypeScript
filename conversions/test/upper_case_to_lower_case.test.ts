8612d98c13256b1e52c5fe6337e9e1a626ca3c93
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-10-01 22:38:09 +0530

    fix : file name

---
+++ conversions/test/upper_case_to_lower_case.test.ts
@@ -0,0 +1,14 @@
+import { toLowerCase } from "../upper_case_to_lower_case";
+
+describe('toLowerCase Function', () => {
+  it.each([
+    ['Hello World', 'hello world'],
+    ['UPPERCASE', 'uppercase'],
+    ['lowercase', 'lowercase'],
+    ['123', '123'],
+    ['', ''],
+  ])('converts "%s" to lowercase as "%s"', (input, expected) => {
+    const result = toLowerCase(input);
+    expect(result).toBe(expected);
+  });
+});
