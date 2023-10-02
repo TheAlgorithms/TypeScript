6416b18c748b58d53e2e05f7a59a9b7736be0843
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-10-02 23:05:25 +0530

    feat : added counting sort method

---
+++ sorts/counting_sort.ts
@@ -0,0 +1,30 @@
+/**
+ * @author : dev-madhurendra
+ * Counting sort is an algorithm for sorting a collection
+ * of objects according to keys that are small integers.
+ * @see: https://en.wikipedia.org/wiki/Counting_sort
+ * @example : 
+ * const array = [3, 0, 2, 5, 4, 1]
+ * countingSort(array, 0, 5)
+ */
+  
+export const countingSort = (inputArr: number[], min: number, max: number) => {
+    const sortedArr = []
+    
+    const count = new Array(max - min + 1).fill(0)
+
+    for (let i = 0; i < inputArr.length; i++)
+      count[inputArr[i] - min]++
+
+    count[0] -= 1
+    
+    for (let i = 1; i < count.length; i++)
+      count[i] += count[i - 1]
+
+    for (let i = inputArr.length - 1; i >= 0; i--) {
+      sortedArr[count[inputArr[i] - min]] = inputArr[i]
+      count[inputArr[i] - min]--
+    }
+
+    return sortedArr
+  }
