2b023a40eb2ef474c5bbbf766147a3f768b41ee5
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2023-10-03 15:03:02 +0200

    Test first 11 ugly numbers

--- maths/test/ugly_numbers.test.ts
+++ maths/test/ugly_numbers.test.ts
@@ -1,12 +1,6 @@
 import { UglyNumbers } from '../ugly_numbers';
 
-test.each([[1, 1], [7, 8], [11, 15]])('Ugly Numbers', (number: number, result: number) => {
-  const uglyNumber = UglyNumbers();
-
-  for(let i = 0; i < (number - 1); i++)
-    uglyNumber.next();
-
-  const e = uglyNumber.next().value;
-  console.log(e);
-  expect(e).toBe(result);
+test('Ugly Numbers', () => {
+  const uglyNumbers = UglyNumbers();
+  expect(Array(11).map((_, i) => uglyNumbers.next())).toEqual([1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15]);
 })
