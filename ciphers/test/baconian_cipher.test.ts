39f547e41bb7854b7bf97c0e1017dfd3c2c6d42a
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2023-10-05 02:22:21 +1100

    Add baconian cipher

---
+++ ciphers/test/baconian_cipher.test.ts
@@ -0,0 +1,22 @@
+import { BaconianCipher } from "../baconian_cipher";
+
+describe("BaconianCipher", () => {
+  let baconianCipher: BaconianCipher = new BaconianCipher();
+
+  it("'Hello world' === baconianDecode(baconianEncode('Hello world'))", () => {
+    const word: string = "Hello world";
+    const expectedResult: string = "HELLO WORLD";
+    const result: string = baconianCipher.baconianDecode(
+      baconianCipher.baconianEncode(word)
+    );
+    expect(result).toMatch(expectedResult);
+  });
+
+  it("'AABBBAABAAABABBABABBABBBA BABBAABBBABAAABABABBAAABB' === baconianEncode(baconianDecode('AABBBAABAAABABBABABBABBBA BABBAABBBABAAABABABBAAABB'))", () => {
+    const word: string = "AABBBAABAAABABBABABBABBBA BABBAABBBABAAABABABBAAABB";
+    const result: string = baconianCipher.baconianEncode(
+      baconianCipher.baconianDecode(word)
+    );
+    expect(result).toMatch(word);
+  });
+});
