86ea97678ca052052f4ea7915e468eb23f0d4c1a
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-10-04 11:51:13 +0530

    feat : added log2 method implementation

---
+++ bit_manipulation/log_two.ts
@@ -0,0 +1,15 @@
+/**
+ * @author dev-madhurendra <https://github.com/dev-madhurendra>
+ * @see https://handwiki.org/wiki/Binary_logarithm
+ * Approximate log2 using bitwise operators
+ * @param {number} n
+ * @returns {number} Log2 approximation equal to floor(log2(n))
+ */
+export const logTwo = (n: number): number => {
+    let result = 0
+    while (n >> 1) {
+      n >>= 1
+      result++
+    }
+    return result
+}
---
+++ bit_manipulation/test/log_two.test.ts
@@ -0,0 +1,10 @@
+import { logTwo } from "../log_two"
+
+describe('LogTwoTests' , () => {
+    test.each([...Array(100).keys()].map(i => [i + 1]))(
+        'log2(%i)',
+        (input) => {
+          expect(logTwo(input)).toBe(Math.floor(Math.log2(input)));
+        }
+    );
+})
