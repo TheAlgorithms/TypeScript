ee5ac4914ee576b540cbb34f669ec49a03c35b70
Rak Laptudirm <rak@laptudirm.com>
2023-10-04 09:57:34 +0530

    chore: fix empty lines

--- sorts/swap_sort.ts
+++ sorts/swap_sort.ts
@@ -21,17 +21,13 @@ export const minSwapsToSort =  (inputArr: number[]): number => {
     for (let i = 0; i < inputArr.length; i++) {
       if (inputArr[i] !== sortedArray[i]) {
         const temp = inputArr[i]
-
         inputArr[i] = inputArr[indexMap.get(sortedArray[i])]
-
         inputArr[indexMap.get(sortedArray[i])] = temp
-  
         indexMap.set(temp,indexMap.get(sortedArray[i]))
-
         indexMap.set(sortedArray[i],1)
-
         swaps++
       }
     }
+
     return swaps
 }
