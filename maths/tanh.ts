26ad4860a2f81554d3f070643c4120820fb4caf9
Kausthub Kannan <kausthubkannan961@gmail.com>
2023-10-05 12:44:39 +0000

    Added tanh function

---
+++ maths/tanh.ts
@@ -0,0 +1,17 @@
+/**
+ * @function TanH
+ * @description Returns the hyperbolic tangent of a number
+ * @summary The hyperbolic tangent (TanH) function is a mathematical function
+ * that returns the hyperbolic tangent of the provided real number.
+ * TanH is defined as (e^x - e^(-x)) / (e^x + e^(-x)), where 'x' is the input.
+ * @param {number} x - The input number
+ * @returns {number} The hyperbolic tangent of 'x'
+ * @see [Wikipedia](https://en.wikipedia.org/wiki/Hyperbolic_function#Hyperbolic_tangent)
+ * @example TanH(0) = 0
+ * @example TanH(1) = 0.7615941559557649
+ * @example TanH(-1) = -0.7615941559557649
+ */
+export const TanH = (x: number): number => {
+    return (Math.exp(x) - Math.exp(-x)) / (Math.exp(x) + Math.exp(-x))
+}
+  
---
+++ maths/test/tanh.test.ts
@@ -0,0 +1,5 @@
+import { TanH } from "../tanh";
+
+test.each([[0, 0], [1, 0.7615941559557649], [-1, -0.7615941559557649],])("The hyperbolic tangent of %i is %f", (x, expected) => {
+  expect(TanH(x)).toBeCloseTo(expected, 10);
+})
