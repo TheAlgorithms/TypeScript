b1094362e92ec1ef40b925efcf099069526b6bf1
Surya Prakash Chaudhary <67498076+Suryac72@users.noreply.github.com>
2023-10-05 15:33:31 +0530

    feat: Added Longest Common Subsequence (#173)

---
+++ dynamic_programming/test/lcs.test.ts
@@ -0,0 +1,28 @@
+import { longestCommonSubsequence } from "../lcs";
+
+describe("longestCommonSubsequence", () => {
+  it("should return the longest common subsequence", () => {
+    expect(longestCommonSubsequence("ABCD", "ACDF")).toBe("ACD");
+
+    expect(longestCommonSubsequence("AGGTAB", "GXTXAYB")).toBe("GTAB");
+
+    expect(longestCommonSubsequence("abcdef", "xyz")).toBe("");
+
+    expect(longestCommonSubsequence("", "")).toBe("");
+  });
+
+  it("should handle cases with spaces and special characters", () => {
+    expect(longestCommonSubsequence("A B C D", "A C D E")).toBe("A C D");
+
+    expect(longestCommonSubsequence("1234$%^", "!@#$%^")).toBe("$%^");
+  });
+
+  it("should handle cases with longer input strings", () => {
+    expect(
+      longestCommonSubsequence(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      )
+    ).toBe("e iumoor it t oeetr ag li.");
+  });
+});
