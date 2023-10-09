ba084c01b54d5b4d802d555551e1621fd1e2e75f
Kausthub Kannan <kausthubkannan961@gmail.com>
2023-10-09 18:06:32 +0000

    Added 'describe' in tanh.test.ts

--- maths/test/tanh.test.ts
+++ maths/test/tanh.test.ts
@@ -1,5 +1,10 @@
 import { TanH } from "../tanh";
 
-test.each([[0, 0], [1, 0.7615941559557649], [-1, -0.7615941559557649],])("The hyperbolic tangent of %i is %f", (x, expected) => {
-  expect(TanH(x)).toBeCloseTo(expected, 10);
-})
+describe("tanh", () => {
+  test.each([[0, 0], [1, 0.7615941559557649], [-1, -0.7615941559557649]])(
+    "The hyperbolic tangent of %i is %f",
+    (x, expected) => {
+      expect(TanH(x)).toBeCloseTo(expected, 10);
+    }
+  );
+});
