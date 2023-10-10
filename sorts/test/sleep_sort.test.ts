b418633ead21a1b6f971056cb449f93a3a446d44
Ansh Tyagi <anshtyagi2222@gmail.com>
2023-10-10 10:37:50 +0530

    removed the console statements

--- sorts/test/sleep_sort.test.ts
+++ sorts/test/sleep_sort.test.ts
@@ -3,13 +3,11 @@ import { sleepSort } from "../sleep_sort";
 describe("Sleep Sort", () => {
   it("should return the array in increasing order for randomly placed numbers", async () => {
     const result = await sleepSort([4, 1, 8, 10, 3, 2, 5, 0, 7, 6, 9]);
-    console.log(result)
     expect(result).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 
   it("should return the array in increasing order for an array sorted in decreasing order", async () => {
     const result = await sleepSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
-    console.log(result)
     expect(result).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 
