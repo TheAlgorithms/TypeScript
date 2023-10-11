362f503afcbac48b90d8efcb770ec3caddf4447f
Surya Prakash Chaudhary <67498076+Suryac72@users.noreply.github.com>
2023-10-11 11:42:32 +0530

    feat: Gaussian Elimination Method (#184)
    * feat(gaussian_elimination)
    
    * fix(gaussian-elimination)

---
+++ maths/gaussian_elimination.ts
@@ -0,0 +1,52 @@
+/**
+ * Solves a system of linear equations using Gaussian Elimination with partial pivoting.
+ *
+ * @param {number[][]} matrix - The augmented matrix representing the system of equations.
+ * @returns {number[]} An array representing the solutions to the equations.
+ */
+export function gaussianElimination(matrix: number[][]): number[] {
+  const result: number[] = new Array(matrix.length);
+
+  function partialPivot(): void {
+    for (let row = 0; row < matrix.length; row++) {
+      let pivotRow = row;
+
+      for (let column = row + 1; column < matrix.length; column++) {
+        if (Math.abs(matrix[column][row]) > Math.abs(matrix[pivotRow][row])) {
+          pivotRow = column;
+        }
+      }
+
+      if (pivotRow !== row) {
+        for (let column = row; column <= matrix.length; column++) {
+          [matrix[row][column], matrix[pivotRow][column]] = [
+            matrix[pivotRow][column],
+            matrix[row][column],
+          ];
+        }
+      }
+
+      for (let column = row + 1; column < matrix.length; column++) {
+        const factor = matrix[column][row] / matrix[row][row];
+        for (let k = row; k <= matrix.length; k++) {
+          matrix[column][k] -= factor * matrix[row][k];
+        }
+      }
+    }
+  }
+
+  function backSubstitute(): void {
+    for (let row = matrix.length - 1; row >= 0; row--) {
+      let sum = 0;
+      for (let column = row + 1; column < matrix.length; column++) {
+        sum += matrix[row][column] * result[column];
+      }
+      result[row] = (matrix[row][matrix.length] - sum) / matrix[row][row];
+    }
+  }
+
+  partialPivot();
+  backSubstitute();
+
+  return result;
+}
---
+++ maths/test/gaussian_elimination.test.ts
@@ -0,0 +1,41 @@
+import { gaussianElimination } from "../gaussian_elimination";
+
+describe("gaussianElimination", () => {
+  it("should solve system of linear equations", () => {
+    const A: number[][] = [
+      [3.0, 2.0, -4.0, 3.0],
+      [2.0, 3.0, 3.0, 15.0],
+      [5.0, -3, 1.0, 14.0],
+    ];
+
+    let solution: number[] = gaussianElimination(A);
+    solution = solution.map((x) => Math.round(x));
+
+    expect(solution.map((x) => Math.round(x))).toEqual([3, 1, 2]);
+  });
+
+  it("should solve a 2x2 system of linear equations", () => {
+    const A: number[][] = [
+      [2.0, 1.0, 5.0],
+      [1.0, -3.0, 6.0],
+    ];
+
+    let solution: number[] = gaussianElimination(A);
+    solution = solution.map((x) => Math.round(x));
+
+    expect(solution.map((x) => Math.round(x))).toEqual([3, -1]);
+  });
+
+  it("should handle a system with no solution", () => {
+    const A: number[][] = [
+      [1.0, 2.0, 3.0, 4.0],
+      [2.0, 4.0, 6.0, 7.0],
+      [3.0, 6.0, 9.0, 10.0],
+    ];
+
+    let solution: number[] = gaussianElimination(A);
+    solution = solution.filter((value) => !isNaN(value));
+
+    expect(solution).toEqual([]);
+  });
+});
