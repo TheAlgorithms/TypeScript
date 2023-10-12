d2d28f03ca39170e7c5d194d939d5e9e2336e9ff
Surya Prakash Chaudhary <67498076+Suryac72@users.noreply.github.com>
2023-10-12 12:01:06 +0530

    feat: bipartite graph (#190)

---
+++ graph/bipartite_graph.ts
@@ -0,0 +1,46 @@
+const dfs = (
+  graph: number[][],
+  colors: number[],
+  node: number,
+  color: number
+): boolean => {
+  if (colors[node] !== 0) {
+    return colors[node] === color;
+  }
+
+  colors[node] = color;
+
+  for (const neighbor of graph[node]) {
+    if (!dfs(graph, colors, neighbor, -color)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+/**
+ * Determines if a given graph is bipartite.
+ *
+ * A Bipartite Graph is a graph whose vertices can be divided into two independent sets, 
+ * U and V such that every edge (u, v) either connects a vertex from U to V or a vertex from 
+ * V to U
+ *
+ * @param {number[][]} graph - The graph represented as an adjacency list.
+ * @returns {boolean} - `true` if the graph is bipartite, `false` otherwise.
+ */
+
+
+export const isBipartite = (graph: number[][]): boolean => {
+  const n: number = graph.length;
+  const colors: number[] = new Array(n).fill(0);
+
+  for (let i = 0; i < n; i++) {
+    if (colors[i] === 0 && !dfs(graph, colors, i, 1)) {
+      return false;
+    }
+  }
+
+  return true;
+};
