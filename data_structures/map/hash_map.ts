78eab4bbb19b4b1d282a6dc4797010fd4cbf4a4a
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2023-10-14 19:28:18 +1100

    chore: Stop exporting an internal class

--- data_structures/map/hash_map.ts
+++ data_structures/map/hash_map.ts
@@ -14,6 +14,25 @@ interface Map<K, V> {
 }
 
 /**
+ * Represents a key-value pair.
+ *
+ * @template K The type of the key.
+ * @template V The type of the value.
+ * @param key The key.
+ * @param value The value.
+ */
+class HashMapEntry<K, V> {
+    key: K;
+    value: V;
+
+    constructor(key: K, value: V) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+
+/**
  * Represents a hash map.
  * Time complexity:
  * - Set, Get, Delete, Has: O(1) on average, O(n) in the worst case.
@@ -230,23 +249,5 @@ export class HashMap<K, V> implements Map<K, V> {
         for (const entry of entries) {
             this.set(entry.key, entry.value);
         }
-    }
-}
-
-/**
- * Represents a key-value pair.
- *
- * @template K The type of the key.
- * @template V The type of the value.
- * @param key The key.
- * @param value The value.
- */
-export class HashMapEntry<K, V> {
-    key: K;
-    value: V;
-
-    constructor(key: K, value: V) {
-        this.key = key;
-        this.value = value;
     }
 }
