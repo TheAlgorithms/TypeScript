0d7aaff93c00e91a2e6a65b7f1ad32b86f975645
britneywwc <britneywwc@gmail.com>
2023-10-18 10:22:42 +1100

    chore: fix camelcasing and linting

--- maths/test/ugly_numbers.test.ts
+++ maths/test/ugly_numbers.test.ts
@@ -1,6 +1,10 @@
-import { UglyNumbers } from '../ugly_numbers';
+import { uglyNumbers } from "../ugly_numbers";
 
-test('Ugly Numbers', () => {
-  const uglyNumbers = UglyNumbers();
-  expect(Array(11).fill(undefined).map(() => uglyNumbers.next().value)).toEqual([1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15]);
-})
+test("Ugly Numbers", () => {
+  const uglyNums = uglyNumbers();
+  expect(
+    Array(11)
+      .fill(undefined)
+      .map(() => uglyNums.next().value)
+  ).toEqual([1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15]);
+});
