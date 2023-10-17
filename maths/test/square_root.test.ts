685f3f3ecb0514dad350233681e135979a7fac26
britneywwc <britneywwc@gmail.com>
2023-10-18 10:14:41 +1100

    test: fixed test suites PR comments

--- maths/test/square_root.test.ts
+++ maths/test/square_root.test.ts
@@ -1,21 +1,28 @@
-import { SquareRoot } from "../square_root";
+import { squareRoot } from "../square_root";
 
-describe("SquareRoot", () => {
+describe("squareRoot", () => {
   test.each([-1, -10, -2.4])(
     "should throw an error for negative numbers",
-    (n) => {
-      expect(() => SquareRoot(n)).toThrow(
-        "number must be non-negative number > 0"
-      );
+    (n: number) => {
+      expect(() => squareRoot(n)).toThrow("number must be non-negative number");
     }
   );
 
-  test("should return correct square root value", () => {
-    expect(SquareRoot(0)).toBe(0);
-    expect(SquareRoot(1)).toBe(1);
-    expect(SquareRoot(4)).toBe(2);
-    expect(SquareRoot(9)).toBe(3);
-    expect(SquareRoot(16)).toBe(4);
-    expect(SquareRoot(25)).toBe(4);
-  });
+  test.each([0, 1, 4, 9, 16, 25])(
+    "should return correct rational square root value",
+    () => {
+      (n: number) => {
+        expect(() => squareRoot(n)).toBeCloseTo(Math.sqrt(n));
+      };
+    }
+  );
+
+  test.each([2, 15, 20, 40, 99, 10032])(
+    "should return correct irrational square root value",
+    () => {
+      (n: number) => {
+        expect(() => squareRoot(n)).toBeCloseTo(Math.sqrt(n));
+      };
+    }
+  );
 });
