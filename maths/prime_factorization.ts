b72e357c244ed059e0c71c6372e33c9e08f54906
Ayoub Chegraoui <ayoubch807@gmail.com>
2023-10-26 14:36:30 +0100

    feat: add prime factorization algorithm (#214)

---
+++ maths/prime_factorization.ts
@@ -0,0 +1,27 @@
+/**
+ * @description Get exponenets of each prime number in factorization of a number n
+ * @param {number} n - A natural number.
+ * @return {Map<number, number>} - factorization of number n.
+ * @see https://en.wikipedia.org/wiki/Integer_factorization
+ * @example factorize(4) = Map {2 => 2}
+ * @example factorize(5) = Map {5 => 1}
+ */
+export const factorize = (n: number): Map<number, number> => {
+    let result: Map<number, number> = new Map();
+
+    for (let i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            let occurence = result.get(i);
+            if (!occurence) occurence = 0;
+            result.set(i, occurence + 1);
+            n = n / i;
+        }
+    }
+    if (n > 1) {
+        let occurence = result.get(n);
+        if (!occurence) occurence = 0;
+        result.set(n, occurence + 1);
+    }
+
+    return result;
+};
