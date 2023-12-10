d75a181fa0d746e495be99107d399f7bf3668841
tamaf96 <tamaf96@mi.fu-berlin.de>
2023-12-10 21:21:05 +0100

    make generic and add tests for string and date

--- sorts/tree_sort.ts
+++ sorts/tree_sort.ts
@@ -2,15 +2,15 @@
  * @author : tamaf96<https://github.com/tamaf96>
  * @description
  *  Tree Sort sorts a list by building a binary search tree and traverse is.
- * @param {number[]} arr - Array of numbers
- * @return {number[]} - The sorted Array.
+ * @param {T[]} arr - Array of comparible items
+ * @return {T[]} - The sorted Array.
  * @see <https://en.wikipedia.org/wiki/Tree_sort>
  */
 
 import { BinarySearchTree } from "../data_structures/tree/binary_search_tree";
 
-export const treeSort = (arr: number[]): number[] => {
-  const searchTree = new BinarySearchTree<number>();
+export const treeSort = <T>(arr: T[]): T[] => {
+  const searchTree = new BinarySearchTree<T>();
   for (const item of arr) {
     searchTree.insert(item);
   }
