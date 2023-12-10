c24aa2068c7cabae13a0e2dda704a411fda15f93
tamaf96 <tamaf96@mi.fu-berlin.de>
2023-12-10 12:18:18 +0100

    use existing data structure

--- sorts/tree_sort.ts
+++ sorts/tree_sort.ts
@@ -7,54 +7,12 @@
  * @see <https://en.wikipedia.org/wiki/Tree_sort>
  */
 
-type BinaryTree<T> = {
-  leftSubTree: BinaryTree<T> | null;
-  node: T | null;
-  rightSubTree: BinaryTree<T> | null;
-};
+import { BinarySearchTree } from "../data_structures/tree/binary_search_tree";
 
 export const treeSort = (arr: number[]): number[] => {
-  const searchTree = emptyTree<number>();
+  const searchTree = new BinarySearchTree<number>();
   for (const item of arr) {
-    insert(searchTree, item);
+    searchTree.insert(item);
   }
-  return inOrder(searchTree);
-};
-
-const emptyTree = <T,>(): BinaryTree<T> => ({
-  leftSubTree: null,
-  node: null,
-  rightSubTree: null,
-});
-
-const insert = (searchTree: BinaryTree<number>, item: number): void => {
-  if (searchTree.node === null) {
-    searchTree.node = item;
-  } else {
-    if (item < searchTree.node) {
-      if (searchTree.leftSubTree === null) {
-        searchTree.leftSubTree = emptyTree<number>();
-      }
-      insert(searchTree.leftSubTree, item);
-    } else {
-      if (searchTree.rightSubTree === null) {
-        searchTree.rightSubTree = emptyTree<number>();
-      }
-      insert(searchTree.rightSubTree, item);
-    }
-  }
-};
-
-const inOrder = (
-  searchTree: BinaryTree<number> | null,
-  arr: number[] = [],
-): number[] => {
-  if (searchTree === null || searchTree.node === null) {
-    return [];
-  } else {
-    inOrder(searchTree.leftSubTree, arr);
-    arr.push(searchTree.node);
-    inOrder(searchTree.rightSubTree, arr);
-  }
-  return arr;
+  return searchTree.inOrderTraversal();
 };
