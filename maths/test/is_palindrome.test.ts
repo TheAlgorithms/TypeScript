fea7c8e6c0d085c4de73660ac4dd882c1e740fc1
Piotr Idzik <vil02@o2.pl>
2024-02-03 18:29:40 +0000

    tests: add missing tests of `isPalindrome`

--- maths/test/is_palindrome.test.ts
+++ maths/test/is_palindrome.test.ts
@@ -1,7 +1,7 @@
 import { isPalindrome } from "../is_palindrome";
 
 describe("isPalindrome", () => {
-  test.each([[5, true], [1234, false], [12321, true], [31343, false]])(
+  test.each([[0, true], [1, true], [5, true], [1234, false], [12321, true], [31343, false], [-1, false], [-11, false], [10, false]])(
     "correct output for %i",
     (nums, expected) => {
         expect(isPalindrome(nums)).toBe(expected);
