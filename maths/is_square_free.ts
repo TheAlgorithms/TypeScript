5460d2f593b40ed56856c89641253a809621fc46
Piotr Idzik <vil02@o2.pl>
2024-02-09 20:50:32 +0000

    fix: handle squares properly in `isSquareFree`

--- maths/is_square_free.ts
+++ maths/is_square_free.ts
@@ -14,7 +14,7 @@ export const isSquareFree = (n: number): boolean => {
     if (n % 2 === 0) n = n / 2;
     if (n % 2 === 0) return false;
 
-    for (let i: number = 3; i < Math.sqrt(n); i = i + 2) {
+    for (let i: number = 3; i <= Math.sqrt(n); i = i + 2) {
         if (n % i === 0) {
             n = n / i;
             if (n % i === 0) return false;
--- maths/test/is_square_free.test.ts
+++ maths/test/is_square_free.test.ts
@@ -1,11 +1,14 @@
 import { isSquareFree } from '../is_square_free';
 
 describe('isSquareFree', () => {
-    test('should return correct boolean value', () => {
-        expect(isSquareFree(1)).toBe(true);
-        expect(isSquareFree(10)).toBe(true);
-        expect(isSquareFree(20)).toBe(false);
-        expect(isSquareFree(26)).toBe(true);
-        expect(isSquareFree(48)).toBe(false);
+  test.each([1, 2, 3, 5, 7, 10, 26, 2*3, 3*5*7, 11*13*17*19])(
+    "%i is square free",
+    (input) => {
+      expect(isSquareFree(input)).toBe(true);
+    });
+  test.each([20, 48, 2*7*7, 2*3*3, 5*5*7, 2*3*13*13*17, 4*4*4, 2*2, 3*3, 5*5, 100, 0])(
+    "%i is not square free",
+    (input) => {
+      expect(isSquareFree(input)).toBe(false);
     });
 });
