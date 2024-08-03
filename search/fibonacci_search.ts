ab771fb5ef0b200d6d4a5c05b61c159fbd496617
Federico Fiaschi <federicofiaschi277@gmail.com>
2024-08-03 17:45:59 +0200

    fix: changed variable names and function return

--- search/fibonacci_search.ts
+++ search/fibonacci_search.ts
@@ -8,49 +8,51 @@
  *
  * @param {number[]} array - sorted list of numbers
  * @param {number} target - target number to search for
- * @return {number} - index of the target number in the list, or -1 if not found
+ * @return {number | null} - index of the target number in the list, or null if not found
  * @see [FibonacciSearch](https://www.geeksforgeeks.org/fibonacci-search/)
  * @example fibonacciSearch([1,2,3], 2) => 1
- * @example fibonacciSearch([4,5,6], 2) => -1
+ * @example fibonacciSearch([4,5,6], 2) => null
  */
 
-export const fibonacciSearch = (array: number[], target: number): number => {
-    const arrayLength = array.length;
-    let fibNMinusTwo = 0;  // (n-2)'th Fibonacci No.
-    let fibNMinusOne = 1;  // (n-1)'th Fibonacci No.
-    let fibN = fibNMinusTwo + fibNMinusOne;  // n'th Fibonacci
-  
-    while (fibN < arrayLength) {
-      fibNMinusTwo = fibNMinusOne;
-      fibNMinusOne = fibN;
-      fibN = fibNMinusTwo + fibNMinusOne;
-    }
-  
-    let offset = -1;
-  
-    while (fibN > 1) {
-      let i = Math.min(offset + fibNMinusTwo, arrayLength - 1);
-  
-      if (array[i] < target) {
-        fibN = fibNMinusOne;
-        fibNMinusOne = fibNMinusTwo;
-        fibNMinusTwo = fibN - fibNMinusOne;
-        offset = i;
-      } else if (array[i] > target) {
-        fibN = fibNMinusTwo;
-        fibNMinusOne = fibNMinusOne - fibNMinusTwo;
-        fibNMinusTwo = fibN - fibNMinusOne;
-      } else {
-        // Element found then return index
-        return i;
-      }
-    }
-  
-    if (fibNMinusOne && array[offset + 1] === target) {
-      return offset + 1;
-    }
-  
-    // Element not found then return -1
-    return -1;
+export const fibonacciSearch = (
+  array: number[],
+  target: number
+): number | null => {
+  const arrayLength = array.length
+  let a = 0;  // (n-2)'th Fibonacci No.
+  let b = 1;  // (n-1)'th Fibonacci No.
+  let c = a + b;  // n'th Fibonacci
+
+  while (c < arrayLength) {
+    a = b;
+    b = c;
+    c = a + b;
   }
-  
+
+  let offset = -1
+
+  while (c > 1) {
+    let i = Math.min(offset + a, arrayLength - 1);
+
+    if (array[i] < target) {
+      c = b;
+      b = a;
+      a = c - b;
+      offset = i;
+    } else if (array[i] > target) {
+      c = a;
+      b = b - a;
+      a = c - b;
+    } else {
+      // Element found then return index
+      return i;
+    }
+  }
+
+  if (b && array[offset + 1] === target) {
+    return offset + 1;
+  }
+
+  // Element not found then return null
+  return null;
+}
