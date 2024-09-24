3f3048120335f22f54537c028dd0bdd096eba71d
hasanalkaf3 <hakalkaf482@gmail.com>
2024-09-24 20:14:52 +0300

    feat: add ternary search algorithm

---
+++ search/ternary_search.ts
@@ -0,0 +1,87 @@
+/* Ternary search is similar to binary search but it divides the sorted array
+ * into three parts and determines which part the key lies in. The array will
+ * be divided into three intervals by using two middle points, mid1 and mid2.
+ * The value of the key will first be compared with the two mid points, the value
+ * will be returned if there is a match. Then, if the value of the key is less
+ * than mid1, narrow the interval to the first part. Else, if the value of the
+ * key is greater than mid2, narrow the interval to the third part. Otherwise,
+ * narrow the interval to the middle part. Repeat the steps until the value is
+ * found or the interval is empty (value not found after checking all elements).
+ *
+ * Reference: https://www.geeksforgeeks.org/ternary-search/
+ */
+
+function ternarySearchRecursive(
+  arr: any[],
+  key: any,
+  low: number = 0,
+  high: number = arr.length - 1
+): number {
+  // if low > high => we have searched the whole array without finding the item
+  if (low > high) return -1
+
+  // find the mid1 and mid2
+  const mid1: number = Math.floor(low + (high - low) / 3)
+  const mid2: number = Math.floor(high - (high - low) / 3)
+
+  // check if key is found at any mid
+  // return index of key if found
+  if (arr[mid1] === key) return mid1
+
+  // return index of key if found
+  if (arr[mid2] === key) return mid2
+
+  // since the key is not found at mid,
+  // check in which region it is present
+  // and repeat the Search operation
+  // in that region
+  if (key < arr[mid1]) {
+    // the key lies in between low and mid1
+    return ternarySearchRecursive(arr, key, low, mid1 - 1)
+  } else if (key > arr[mid2]) {
+    // the key lies in between mid2 and high
+    return ternarySearchRecursive(arr, key, mid2 + 1, high)
+  } else {
+    // the key lies in between mid1 and mid2
+    return ternarySearchRecursive(arr, key, mid1 + 1, mid2 - 1)
+  }
+}
+
+function ternarySearchIterative(
+  arr: any[],
+  key: any,
+  low: number = 0,
+  high: number = arr.length - 1
+): number {
+  while (high >= low) {
+    // find the mid1 and mid2
+    const mid1: number = Math.floor(low + (high - low) / 3)
+    const mid2: number = Math.floor(high - (high - low) / 3)
+
+    // check if key is found at any mid
+    // return index of key if found
+    if (arr[mid1] === key) return mid1
+
+    // return index of key if found
+    if (arr[mid2] === key) return mid2
+
+    // since the key is not found at mid,
+    // check in which region it is present
+    // and repeat the Search operation
+    // in that region
+    if (key < arr[mid1]) {
+      // the key lies in between low and mid1
+      high = mid1 - 1
+    } else if (key > arr[mid2]) {
+      // the key lies in between mid2 and high
+      low = mid2 + 1
+    } else {
+      // the key lies in between mid1 and mid2
+      ;[low, high] = [mid1 + 1, mid2 - 1]
+    }
+  }
+  // the key was not found
+  return -1
+}
+
+export { ternarySearchRecursive, ternarySearchIterative }
