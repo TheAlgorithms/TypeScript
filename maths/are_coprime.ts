086a20ec8780559b42fd0482d1392232e5021dce
Ambarish-2002 <amb.kshirsagar@gmail.com>
2024-09-25 01:10:15 -0700

    Update are_coprime.ts

--- maths/are_coprime.ts
+++ maths/are_coprime.ts
@@ -1,3 +1,12 @@
+/**
+ * @function areCoprime
+ * @description Checks if two given numbers are co-prime or not
+ * @param {a} number - a numeric input value
+ * @param {a} number - a numeric input value
+ * @example areCoprime(2,4) = false
+ * @example areCoprime(2,3) = true
+ */
+
 import { greatestCommonFactor } from "./greatest_common_factor"
 
 export const areCoprime = (a: number, b: number): boolean =>{
